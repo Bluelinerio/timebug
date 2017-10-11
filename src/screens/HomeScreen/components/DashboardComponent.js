94b328b948b0a5c5f570d53d0ab415f11380e2b4
Amos Elmaliah <amosel@gmail.com>
2017-10-11 19:42:12 -0400

    Recycle vector-icons for now for the clock. + Pixel perfect adjustments.

--- src/screens/HomeScreen/components/DashboardComponent.js
+++ src/screens/HomeScreen/components/DashboardComponent.js
@@ -1,5 +1,4 @@
 // @flow
-
 import React, { Component } from 'react';
 import {
   Image,
@@ -18,54 +17,97 @@ type Props = {
   allSteps: IStep[],
   currentStep: IStep,
   navigate(): any
-}
+};
 
 type State = {
   refreshing: boolean
-}
+};
 
-const headerBackground = require('../../../resources/images/home_background.jpg');
-const buttonIcon       = require('../../../resources/images/clock_icon.png');
+const headerBackground = require("../../../resources/images/home_background.jpg");
 
 export default class DashboardComponent extends Component<Props, State> {
   render() {
-    let {
-          currentStep,
-          totalNumberOfSteps,
-        } = this.props;
+    let { currentStep, totalNumberOfSteps } = this.props;
 
-    return <ScrollableHeader
-      headerImage={headerBackground}
-      header={
-        <View style={[ styles.HomeScreenHeader, styles.headerColor ]}>
-          <View style={styles.HomeScreenChallengeInfo}>
-            <CustomImage
-              style={styles.HomeScreenHeaderImage}
-              imageUri={getImageUrl(currentStep.shortIcon)}
-            />
-            <View style={styles.HomeScreenHeaderText}>
-              <Text style={styles.HomeScreenHeaderTitle}>Next challenge</Text>
-              <Text style={[ styles.HomeScreenTitle ]}>{currentStep.type}</Text>
-              <Text style={[ styles.HomeScreenLittleText ]}>
-                {currentStep.stepScreenDescription}
+    return (
+      <ScrollableHeader
+        headerImage={headerBackground}
+        header={
+          <View style={[styles.HomeScreenHeader, styles.headerColor]}>
+            <View style={styles.HomeScreenChallengeInfo}>
+              <CustomImage
+                style={styles.HomeScreenHeaderImage}
+                imageUri={getImageUrl(currentStep.shortIcon)}
+              />
+              <View style={styles.HomeScreenHeaderText}>
+                <Text style={styles.HomeScreenHeaderTitle}>Next challenge</Text>
+                <Text style={[styles.HomeScreenTitle]}>{currentStep.type}</Text>
+                <Text style={[styles.HomeScreenLittleText]}>
+                  {currentStep.stepScreenDescription}
+                </Text>
+                <Text style={[styles.HomeScreenStep]}>
+                  STEP # {currentStep.number}/{totalNumberOfSteps}
+                </Text>
+              </View>
+            </View>
+            <Button
+              containerStyle={{
+                marginTop: 40,
+                backgroundColor: "white",
+                height: 45,
+                minWidth: 240,
+                paddingHorizontal: 10,
+                borderRadius: 150,
+                alignSelf: "center",
+                justifyContent: "center"
+              }}
+              onPress={() =>
+                this.props.goToTextScreen({ number: currentStep.number })}
+            >
+              <View
+                style={{
+                  flex: 1,
+                  flexDirection: "row",
+                  alignItems: "flex-end"
+                }}
+              >
+                <Feather
+                  name="clock"
+                  size={34}
+                  style={{
+                    color: "#003681",
+                    marginTop: 2
+                  }}
+                />
+                <Text
+                  style={{
+                    fontFamily: "Helvetica",
+                    color: "#003681",
+                    fontWeight: "bold",
+                    fontSize: 10,
+                    marginBottom: 6
+                  }}
+                >
+                  {currentStep.duration}min
+                </Text>
+              </View>
+              <Text
+                style={{
+                  fontFamily: "Helvetica",
+                  fontSize: 15.5,
+                  fontWeight: "bold",
+                  textAlign: "left",
+                  color: "#003681",
+                  flex: 2,
+                  marginLeft: 15
+                }}
+              >
+                START
               </Text>
-              <Text style={[ styles.HomeScreenStep ]}>STEP
-                # {currentStep.number}/{totalNumberOfSteps}</Text>
-            </View>
+            </Button>
           </View>
-          <Button
-            containerStyle={styles.HomeScreenWideButton}
-            onPress={() => this.props.goToTextScreen({ number: currentStep.number })}
-          >
-            <View style={styles.HomeScreenAbsoluteContainer}>
-              <Image source={buttonIcon} style={styles.HomeScreenButtonImage}/>
-              <Text style={[ styles.HomeScreenDurationText ]}>{currentStep.duration}min</Text>
-
-            </View>
-            <Text style={styles.HomeScreenWideButtonText}>START</Text>
-          </Button>
-        </View>
-      }
-    />;
+        }
+      />
+    );
   }
 }
