7ebbb977241dfc5e8919f8f6ab10516a01fd2cac
Amos Elmaliah <amosel@gmail.com>
2017-10-12 11:55:23 -0400

    Fixed namspace issue with the  word 'reset' being used bother for an arguement and a imported function

--- src/sagas/navigate.saga.js
+++ src/sagas/navigate.saga.js
@@ -1,37 +1,47 @@
 // @flow
 
-import { takeLatest }       from 'redux-saga/effects';
+import { takeLatest } from 'redux-saga/effects';
 import {
-  GO_TO_ASSIGNMENTS_SCREEN,
-  GO_TO_CONGRATULATIONS_SCREEN,
-  GO_TO_HOME_SCREEN,
-  GO_TO_TEXT_SCREEN,
-  GO_TO_WORKBOOK_SCREEN,
-}                           from '../constants/actionTypes';
-import { navigate }         from '../HOC/navigation'
-import type { IAssignment } from '../interfaces';
+	GO_TO_ASSIGNMENTS_SCREEN,
+	GO_TO_CONGRATULATIONS_SCREEN,
+	GO_TO_HOME_SCREEN,
+	GO_TO_TEXT_SCREEN,
+	GO_TO_WORKBOOK_SCREEN
+} from '../constants/actionTypes';
+import * as navigation from '../HOC/navigation';
+import type, { IAssignment } from '../interfaces';
+
+const _goToHomeScreen = (action: { reset: boolean, number: number }) => {
+	debugger;
+	if (action.reset) {
+		navigation.reset('HomeScreen', action);
+	} else {
+		navigation.navigate('HomeScreen', action);
+	}
+};
 
 export function* goToHomeScreen() {
-  yield takeLatest(GO_TO_HOME_SCREEN, (action: { number: number }) => {
-    if (action.reset) {
-      return reset('HomeScreen', action)
-    }
-    navigate('HomeScreen', action)
-  });
+	yield takeLatest(GO_TO_HOME_SCREEN, _goToHomeScreen);
 }
 
 export function* goToTextScreen() {
-  yield takeLatest(GO_TO_TEXT_SCREEN, (action: { number: number }) => navigate('TextScreen', action));
+	yield takeLatest(GO_TO_TEXT_SCREEN, (action: { number: number }) => navigation.navigate('TextScreen', action));
 }
 
 export function* goToAssignmentsScreen() {
-  yield takeLatest(GO_TO_ASSIGNMENTS_SCREEN, (action: { number: number, assignments: IAssignment[] }) => navigate('AssignmentsScreen', action));
+	yield takeLatest(GO_TO_ASSIGNMENTS_SCREEN, (action: { number: number, assignments: IAssignment[] }) =>
+		navigation.navigate('AssignmentsScreen', action)
+	);
 }
 
 export function* goToWorkBookScreen() {
-  yield takeLatest(GO_TO_WORKBOOK_SCREEN, (action: { number: number }) => navigate('WorkBookScreen', action));
+	yield takeLatest(GO_TO_WORKBOOK_SCREEN, (action: { number: number }) =>
+		navigation.navigate('WorkBookScreen', action)
+	);
 }
 
 export function* goToCongratulationsScreen() {
-  yield takeLatest(GO_TO_CONGRATULATIONS_SCREEN, (action: { number: number }) => navigate('CongratulationsScreen', action));
+	yield takeLatest(GO_TO_CONGRATULATIONS_SCREEN, (action: { number: number }) =>
+		navigation.navigate('CongratulationsScreen', action)
+	);
 }
