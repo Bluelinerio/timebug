11124a6540045c42864d4471781f2c34f30f421a
Amos Elmaliah <amosel@gmail.com>
2017-10-21 15:54:01 -0400

    Fix content for credentials.

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,2 +1,4 @@
-export const CONTENTFUCONTENT_DELIVERY_API_ACCESS_TOKENL_ACCESS_TOKEN = 'c139e7f2a7a86fc0813e71fbb18bb7b1921189ce4d7cc58c7f0ccc0022adee5f';
-export const CONTENTFUL_SPACE        = '1gbed7lrsmj4';
+export const CONTENTFUL_CREDENTIALS = {
+  accessToken: 'c139e7f2a7a86fc0813e71fbb18bb7b1921189ce4d7cc58c7f0ccc0022adee5f',
+  space: '1gbed7lrsmj4',  
+};
--- src/contentful.js
+++ src/contentful.js
@@ -1,9 +1,6 @@
 // @flow
 
 import { createClient }                               from 'contentful'
-import { CONTENT_DELIVERY_API_ACCESS_TOKEN, CONTENTFUL_SPACE, } from "./constants/config"
+import { CONTENTFUL_CREDENTIALS }                     from "./constants/config"
 
-export const contentfulClient = createClient({
-  accessToken: CONTENT_DELIVERY_API_ACCESS_TOKEN,
-  space: CONTENTFUL_SPACE,
-});
+export const contentfulClient = createClient(CONTENTFUL_CREDENTIALS);
