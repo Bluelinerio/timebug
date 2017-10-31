863a02488107eb631ee9b256fee43157f5473b1e
Amos Elmaliah <amosel@gmail.com>
2017-10-31 13:20:09 -0400

    Set up paths for deep linking in main navigator.

--- src/navigation/navigator.js
+++ src/navigation/navigator.js
@@ -1,36 +1,59 @@
-import React                      from 'react';
-import { StackNavigator, }        from 'react-navigation';
-import CardStackStyleInterpolator from '../utils/CustomCardStackStyleInterpolator'
-import HomeScreen                 from "../screens/HomeScreen";
-import TextScreen                 from "../screens/TextScreen";
-import AssignmentsScreen          from "../screens/AssignmentsScreen";
-import CongratulationsScreen      from "../screens/CongratulationsScreen";
-import WorkBookScreen             from "../screens/WorkBookScreen";
+import React from "react";
+import { StackNavigator } from "react-navigation";
+import CardStackStyleInterpolator from "../utils/CustomCardStackStyleInterpolator";
+import HomeScreen from "../screens/HomeScreen";
+import TextScreen from "../screens/TextScreen";
+import AssignmentsScreen from "../screens/AssignmentsScreen";
+import CongratulationsScreen from "../screens/CongratulationsScreen";
+import WorkBookScreen from "../screens/WorkBookScreen";
 
-export const initialRouteName = 'HomeScreen';
+export const initialRouteName = "HomeScreen";
 
-const Navigator = StackNavigator({
-    HomeScreen,
-    TextScreen,
-    AssignmentsScreen,
-    CongratulationsScreen,
-    WorkBookScreen,
+// TODO: there's an issue with moving from the current setup where the import of each screen gets you an object that looks like { screen: } rather than a component, so I added 
+const Navigator = StackNavigator(
+  {
+    HomeScreen : {
+      screen: HomeScreen.screen,
+    } ,
+    TextScreen: {
+      screen: TextScreen.screen,
+      path: 'step/:number'
+    },
+    AssignmentsScreen: {
+      screen: AssignmentsScreen.screen,
+      path: 'step/leadin/:number'
+    },
+    CongratulationsScreen: {
+      screen: CongratulationsScreen.screen,
+      path: 'step/finished/:number'
+    },
+    WorkBookScreen: {
+      screen: WorkBookScreen.screen,
+      path: 'step/workbook/:number'
+    },
   },
   {
     initialRouteName,
-    headerMode: 'screen',
+    headerMode: "screen",
     cardStyle: {
-      backgroundColor: 'white',
-      opacity: 1,
+      backgroundColor: "white",
+      opacity: 1
     },
-    transitionConfig: () => ( {
-      screenInterpolator: (sceneProps) => {
-        if ([ 'TextScreen', 'HomeScreen' ].indexOf(sceneProps.navigation.state.routes[ sceneProps.navigation.state.routes.length - 1 ].routeName) !== -1) {
-          return CardStackStyleInterpolator.forVertical(sceneProps)
+    transitionConfig: () => ({
+      screenInterpolator: sceneProps => {
+        if (
+          ["TextScreen", "HomeScreen"].indexOf(
+            sceneProps.navigation.state.routes[
+              sceneProps.navigation.state.routes.length - 1
+            ].routeName
+          ) !== -1
+        ) {
+          return CardStackStyleInterpolator.forVertical(sceneProps);
         }
-        return CardStackStyleInterpolator.forHorizontal(sceneProps)
-      },
-    } ),
-  });
+        return CardStackStyleInterpolator.forHorizontal(sceneProps);
+      }
+    })
+  }
+);
 
 export default Navigator;
