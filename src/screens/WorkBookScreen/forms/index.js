49a2deb313931897fe605f28e97c46c98850d11c
Amos Elmaliah <amosel@gmail.com>
2017-11-01 12:26:02 -0400

    Initial work by Dan

--- src/screens/WorkBookScreen/forms/index.js
+++ src/screens/WorkBookScreen/forms/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import t     from "../components/templates";
 import step1 from './step1';
 import step2 from './step2';
+import step3 from './step3';
 
 export default {
   1: step1,
---
+++ src/screens/WorkBookScreen/forms/step3.js
@@ -0,0 +1,83 @@
+import t from "../components/templates";
+import { CharachterStrengths, CharachterWeaknesses } from "./contents";
+
+export default {
+  1: {
+    title: "List 3-5 Charachter Strengths of yours, and the resulting goals that you achieved.",
+    type: t.struct({
+      field: t.list(
+        t.struct({
+         })
+      )
+    }),
+    options: {
+      fields: {
+        field: {
+          auto: 'placeholders',
+          disableOrder: true,
+          maxLines: 10,
+          config: {
+            maxLines: 10,
+          },
+        },
+      }
+    }
+  },
+  2: {
+    title: "List 3-5 Charachter Traits that have held you back from achieving your goals.",
+    type: t.struct({
+      field: t.list(
+        t.struct({
+          weaknessSelfView: CharachterWeaknesses,
+          goalsNotReached: t.String
+         })
+      )
+    }),
+    options: {
+      fields: {
+        field: {
+          auto: 'placeholders',
+          disableOrder: true,
+          maxLines: 5,
+          config: {
+            maxLines: 5,
+          },
+        },
+      }
+    }
+  },
+  3: {
+    title: "List 3-5 Charachter Strengths from a friend or family member's point of view.",
+    type: t.struct({
+        friendsName: t.String,
+        strengthFriendView: t.list(CharachterStrengths),
+        weaknessFriendView: t.list(CharachterWeaknesses)
+      }),
+      options: {
+        fields: {
+          friendsName: {
+            label: "Write down your friend or family member's name."
+          },
+          strengthFriendView: {
+            label:"Select 3-5 charachter strengths from your friend or family member's point of view.",
+            minLines:3,
+            maxLines: 5,
+            config: {
+              minLines:3,
+              maxLines: 5,
+            },
+            
+        },
+          weaknessFriendView: {
+            label:"Select 3-5 charachter weaknesses from your friend or family member's point of view.",
+            minLines:3,
+            maxLines: 5,
+            config: {
+              minLines:3,
+              maxLines: 5,
+            },
+          }
+        }
+      }
+  }
+};
