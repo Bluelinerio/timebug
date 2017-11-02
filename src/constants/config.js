638a3e5e5d849c5b98b53b887a00c6f5fec822ef
Oscar <oscar.colmenares@emqu.net>
2017-11-02 17:21:34 -0400

    Set up new heroku endpoint!

--- src/constants/config.js
+++ src/constants/config.js
@@ -3,10 +3,11 @@ export const CONTENTFUL_CREDENTIALS = {
   space: '1gbed7lrsmj4',  
 };
 
-
+const herokuAddr = "https://server-2020.herokuapp.com/"
 export const APOLLO_ENDPOINT = 'https://infinite-dawn-72189.herokuapp.com/'; 
 // 'http://localhost:3000/'
 // 'http://2020-test.local.zaraffasoft.com/'
+
 
 export const APOLLO_CONFIG = {
   uri: APOLLO_ENDPOINT,
