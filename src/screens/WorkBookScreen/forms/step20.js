6ada0b3c4eb51fa17baddd51795a4e7bd348855f
Dan Kurfirst <dan@codacity.com>
2017-11-11 08:21:07 -0500

    step 20 completed

--- src/screens/WorkBookScreen/forms/step20.js
+++ src/screens/WorkBookScreen/forms/step20.js
@@ -20,7 +20,7 @@ export default {
               fields: {
                 goal: {
                   label:'Review your goals, time spent, and key achievements listed on Day 11. What, if anything, has changed with your time and energy allotment?',
-                  placholder:'goal'
+                  auto:'placerholders'
                 },
                 newInsights: {
                   placeholder:'New insights(be as technical as you can)',
@@ -75,12 +75,12 @@ export default {
         relationship:t.Number,
         financial:t.Number,
         placeAndEnvironment:t.Number,
-        spirituality:t.Number
+        spirituality:t.maybe(t.Number)
       }),
       options:{
         fields: {
         rank1:{
-            label: 'Rank the 7 Areas in terms of the order of importance you gave them over the past 5 years.'
+            label: 'Rank the 7 Areas in terms of the order of importance you gave them over the past 5 years.\nRank 1:'
             
         },
         rank2:{
@@ -102,7 +102,8 @@ export default {
           auto:'labels'
         },
         career: {
-          label: 'If the 7 areas were a pie, how would you slice up your energy and motivation level?(Total should = 100%)'
+          label: 'If the 7 areas were a pie, how would you slice up your energy and motivation level?(Total should = 100%)',
+          placeholder:'Career'
         },
         personalityAndHobbies:{
           auto:'placeholders'
@@ -124,6 +125,470 @@ export default {
         }
         }
           }
+      },
+      4: {
+        title:'Give yourself a final evaluation for each of the past 5-years, as well as an overall 5-Year Score(1-10 score per year, for a total maximum high score of 50/50, per each of the 7 Areas that were assessed).',
+        type:t.struct({
+          career2011:t.Number,
+          personalityAndHobbies2011:t.Number,
+          health2011:t.Number,
+          relationship2011:t.Number,
+          financial2011:t.Number,
+          placeAndEnvironment2011:t.Number,
+          spirituality2011:t.Number,
+          career2012:t.Number,
+          personalityAndHobbies2012:t.Number,
+          health2012:t.Number,
+          relationship2012:t.Number,
+          financial2012:t.Number,
+          placeAndEnvironment2012:t.Number,
+          spirituality2012:t.Number,
+          career2013:t.Number,
+          personalityAndHobbies2013:t.Number,
+          health2013:t.Number,
+          relationship2013:t.Number,
+          financial2013:t.Number,
+          placeAndEnvironment2013:t.Number,
+          spirituality2013:t.Number,
+          career2014:t.Number,
+          personalityAndHobbies2014:t.Number,
+          health2014:t.Number,
+          relationship2014:t.Number,
+          financial2014:t.Number,
+          placeAndEnvironment2014:t.Number,
+          spirituality2014:t.Number,
+          career2015:t.Number,
+          personalityAndHobbies2015:t.Number,
+          health2015:t.Number,
+          relationship2015:t.Number,
+          financial2015:t.Number,
+          placeAndEnvironment2015:t.Number,
+          spirituality2015:t.Number,
+          careerOverall:t.Number,
+          personalityAndHobbiesOverall:t.Number,
+          healthOverall:t.Number,
+          relationshipOverall:t.Number,
+          financialOverall:t.Number,
+          placeAndEnvironmentOverall:t.Number,
+          spiritualityOverall:t.maybe(t.Number)
+         }),
+         options: {
+           fields: {
+             
+             career2011:{
+               label:'2011',
+               placeholder:'Career 2011'
+             },
+             personalityAndHobbies2011: {
+               placeholder:'Personality/Hobbies 2011',
+               auto:'none'
+             },
+             health2011: {
+             
+               placeholder:'Health 2011',
+               auto:'none'
+             },
+             relationship2011: {
+        
+              placeholder:'Relationship 2011',
+              auto:'none'
+            },
+            financial2011: {
+              
+              placeholder:'Financial 2011',
+              auto:'none'
+            },
+            placeAndEnvironment2011: {
+           
+              placeholder:'P & E 2011',
+              auto:'none'
+            },
+            spirituality2011: {
+
+              placeholder:'Spirituality 2011',
+              auto:'none'
+            },
+             career2012:{
+              label:'2012',
+              placeholder:'Career 2012'
+            },
+            personalityAndHobbies2012: {
+              placeholder:'Personality/Hobbies 2012',
+              auto:'none'
+            },
+            health2012: {
+              placeholder:'Health 2012',
+              auto:'none'
+            },
+            relationship2012: {
+              placeholder:'Relationship 2012',
+              auto:'none'
+           },
+           financial2012: {
+            placeholder:'Financial 2012',
+            auto:'none'
+           },
+           placeAndEnvironment2012: {
+            placeholder:'P & E 2012',
+            auto:'none'
+           },
+           spirituality2012: {
+            placeholder:'Spirituality 2012',
+            auto:'none'
+           },
+            career2013:{
+              label:'2013',
+              placeholder:'Career 2013'
+            },
+            personalityAndHobbies2013: {
+             
+              placeholder:'Personality/Hobbies 2013',
+              auto:'none'
+            },
+            health2013: {
+     
+              placeholder:'Health 2013',
+              auto:'none'
+            },
+            relationship2013: {
+         
+             placeholder:'Relationship 2013',
+             auto:'none'
+           },
+           financial2013: {
+      
+             placeholder: 'Financial 2013',
+             auto:'none'
+           },
+           placeAndEnvironment2013: {
+             placeholder: 'P & E 2013',
+             auto:'none'
+             
+           },
+           spirituality2013: {
+             placeholder: 'Spirituality 2013',
+             auto:'none'
+           },
+            career2014:{
+              label:'2014',
+              placeholder:'Career 2014'
+            },
+            personalityAndHobbies2014: {
+              placeholder:'Personality/Hobbies 2014',
+              auto:'none'
+            },
+            health2014: {
+              placeholder:'Health 2014',
+              auto:'none'
+            },
+            relationship2014: {
+              placeholder:'Relationship 2014',
+              auto:'none'
+           },
+           financial2014: {
+            placeholder:'Financial 2014',
+            auto:'none'
+           },
+           placeAndEnvironment2014: {
+            placeholder:'P & E 2014',
+            auto:'none'
+           },
+           spirituality2014: {
+            placeholder:'Spirituality 2014',
+            auto:'none'
+           },
+            career2015:{
+              label:'2015',
+              placeholder:'Career 2015'
+            },
+            personalityAndHobbies2015: {
+              placeholder:'Personality/Hobbies 2015',
+              auto:'none'
+            },
+            health2015: {
+              placeholder:'Health 2015',
+              auto:'none'
+            },
+            relationship2015: {
+              placeholder:'Relationship 2015',
+              auto:'none'
+           },
+           financial2015: {
+            placeholder:'Financial 2015',
+            auto:'none'
+           },
+           placeAndEnvironment2015: {
+            placeholder:'P & E 2015',
+            auto:'none'
+           },
+           spirituality2015: {
+            placeholder:'Spirituality 2015',
+            auto:'none'
+           },
+            careerOverall:{
+              label:'Overall',
+              placeholder:'Career Overall'
+            },
+            personalityAndHobbiesOverall:{
+              placeholder:'Personality/Hobbies Overall',
+              auto:'none'
+            },
+            healthOverall: {
+              placeholder:'Health Overall',
+              auto:'none'
+            },
+            relationshipOverall: {
+              placeholder:'Relationship Overall',
+              auto:'none'
+            },
+            financialOverall: {
+              placeholder:'Financial Overall',
+              auto:'none'
+            },
+            placeAndEnvironmentOverall: {
+              placeholder:'P & E Overall',
+              auto:'none'
+            },
+            spiritualityOverall: {
+              placeholder:'Spirituality Overall',
+              auto:'none'
+            }
+
+           }
+         }
+
+      },
+      5:{
+        title:'Have one other person(per Area) - be it a family member or friend, and preferably one of your CEO of Me Board Members - give you a Score for the past 5 years(on a 1-10 scale), and then multiply that number by 5, to arrive at your 3rd Party Rating(this number will be between 5-50; and you should have one for each of the 7 Assessment Areas).',
+        type:t.struct({
+          career2011:t.Number,
+          personalityAndHobbies2011:t.Number,
+          health2011:t.Number,
+          relationship2011:t.Number,
+          financial2011:t.Number,
+          placeAndEnvironment2011:t.Number,
+          spirituality2011:t.Number,
+          career2012:t.Number,
+          personalityAndHobbies2012:t.Number,
+          health2012:t.Number,
+          relationship2012:t.Number,
+          financial2012:t.Number,
+          placeAndEnvironment2012:t.Number,
+          spirituality2012:t.Number,
+          career2013:t.Number,
+          personalityAndHobbies2013:t.Number,
+          health2013:t.Number,
+          relationship2013:t.Number,
+          financial2013:t.Number,
+          placeAndEnvironment2013:t.Number,
+          spirituality2013:t.Number,
+          career2014:t.Number,
+          personalityAndHobbies2014:t.Number,
+          health2014:t.Number,
+          relationship2014:t.Number,
+          financial2014:t.Number,
+          placeAndEnvironment2014:t.Number,
+          spirituality2014:t.Number,
+          career2015:t.Number,
+          personalityAndHobbies2015:t.Number,
+          health2015:t.Number,
+          relationship2015:t.Number,
+          financial2015:t.Number,
+          placeAndEnvironment2015:t.Number,
+          spirituality2015:t.Number,
+          careerOverall:t.Number,
+          personalityAndHobbiesOverall:t.Number,
+          healthOverall:t.Number,
+          relationshipOverall:t.Number,
+          financialOverall:t.Number,
+          placeAndEnvironmentOverall:t.Number,
+          spiritualityOverall:t.maybe(t.Number)
+         }),
+         options: {
+           fields: {
+             
+             career2011:{
+               label:'2011',
+               placeholder:'Career 2011'
+             },
+             personalityAndHobbies2011: {
+               placeholder:'Personality/Hobbies 2011',
+               auto:'none'
+             },
+             health2011: {
+             
+               placeholder:'Health 2011',
+               auto:'none'
+             },
+             relationship2011: {
+        
+              placeholder:'Relationship 2011',
+              auto:'none'
+            },
+            financial2011: {
+              
+              placeholder:'Financial 2011',
+              auto:'none'
+            },
+            placeAndEnvironment2011: {
+           
+              placeholder:'P & E 2011',
+              auto:'none'
+            },
+            spirituality2011: {
+
+              placeholder:'Spirituality 2011',
+              auto:'none'
+            },
+             career2012:{
+              label:'2012',
+              placeholder:'Career 2012'
+            },
+            personalityAndHobbies2012: {
+              placeholder:'Personality/Hobbies 2012',
+              auto:'none'
+            },
+            health2012: {
+              placeholder:'Health 2012',
+              auto:'none'
+            },
+            relationship2012: {
+              placeholder:'Relationship 2012',
+              auto:'none'
+           },
+           financial2012: {
+            placeholder:'Financial 2012',
+            auto:'none'
+           },
+           placeAndEnvironment2012: {
+            placeholder:'P & E 2012',
+            auto:'none'
+           },
+           spirituality2012: {
+            placeholder:'Spirituality 2012',
+            auto:'none'
+           },
+            career2013:{
+              label:'2013',
+              placeholder:'Career 2013'
+            },
+            personalityAndHobbies2013: {
+             
+              placeholder:'Personality/Hobbies 2013',
+              auto:'none'
+            },
+            health2013: {
+     
+              placeholder:'Health 2013',
+              auto:'none'
+            },
+            relationship2013: {
+         
+             placeholder:'Relationship 2013',
+             auto:'none'
+           },
+           financial2013: {
+      
+             placeholder: 'Financial 2013',
+             auto:'none'
+           },
+           placeAndEnvironment2013: {
+             placeholder: 'P & E 2013',
+             auto:'none'
+             
+           },
+           spirituality2013: {
+             placeholder: 'Spirituality 2013',
+             auto:'none'
+           },
+            career2014:{
+              label:'2014',
+              placeholder:'Career 2014'
+            },
+            personalityAndHobbies2014: {
+              placeholder:'Personality/Hobbies 2014',
+              auto:'none'
+            },
+            health2014: {
+              placeholder:'Health 2014',
+              auto:'none'
+            },
+            relationship2014: {
+              placeholder:'Relationship 2014',
+              auto:'none'
+           },
+           financial2014: {
+            placeholder:'Financial 2014',
+            auto:'none'
+           },
+           placeAndEnvironment2014: {
+            placeholder:'P & E 2014',
+            auto:'none'
+           },
+           spirituality2014: {
+            placeholder:'Spirituality 2014',
+            auto:'none'
+           },
+            career2015:{
+              label:'2015',
+              placeholder:'Career 2015'
+            },
+            personalityAndHobbies2015: {
+              placeholder:'Personality/Hobbies 2015',
+              auto:'none'
+            },
+            health2015: {
+              placeholder:'Health 2015',
+              auto:'none'
+            },
+            relationship2015: {
+              placeholder:'Relationship 2015',
+              auto:'none'
+           },
+           financial2015: {
+            placeholder:'Financial 2015',
+            auto:'none'
+           },
+           placeAndEnvironment2015: {
+            placeholder:'P & E 2015',
+            auto:'none'
+           },
+           spirituality2015: {
+            placeholder:'Spirituality 2015',
+            auto:'none'
+           },
+            careerOverall:{
+              label:'Overall',
+              placeholder:'Career Overall'
+            },
+            personalityAndHobbiesOverall:{
+              placeholder:'Personality/Hobbies Overall',
+              auto:'none'
+            },
+            healthOverall: {
+              placeholder:'Health Overall',
+              auto:'none'
+            },
+            relationshipOverall: {
+              placeholder:'Relationship Overall',
+              auto:'none'
+            },
+            financialOverall: {
+              placeholder:'Financial Overall',
+              auto:'none'
+            },
+            placeAndEnvironmentOverall: {
+              placeholder:'P & E Overall',
+              auto:'none'
+            },
+            spiritualityOverall: {
+              placeholder:'Spirituality Overall',
+              auto:'none'
+            }
+
+           }
+         }
+
       }
           
 };
