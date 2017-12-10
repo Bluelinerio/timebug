aeab1b23b8331ef99e40e60ebfa683a2cc97f9a3
Amos Elmaliah <amosel@gmail.com>
2017-12-09 20:16:03 -0500

    Added Apollo Models and new api.

--- src/clients/apollo/index.js
+++ src/clients/apollo/index.js
@@ -1,18 +1,75 @@
-import {
-  ApolloClient,
-  createNetworkInterface
-} from 'react-apollo';
-import { APOLLO_CONFIG } from '../../constants/config';
+// @flow
 
+import { ApolloClient, createNetworkInterface } from 'react-apollo'
+import { APOLLO_CONFIG } from '../../constants/config'
+import type { Auth, User, ApolloUserResponse } from './models'
 
-export * from './mutations/user';
-
-export const networkInterface = createNetworkInterface(APOLLO_CONFIG);
-
-const reduxRootSelector = (state) => {
-  return state;
-};
+const reduxRootSelector = state => {
+	return state
+}
 
 export const client = new ApolloClient({
-  networkInterface
-});
+	createNetworkInterface(APOLLO_CONFIG)
+})
+
+export const fetchUserWithId = (userId: string) =>
+	client
+		.query({
+			query: getUser,
+			fetchPolicy: 'network-only',
+			variables: {
+				id: userId
+			}
+		})
+		.then(graphResponse => graphResponse.data.getUser)
+
+export const authenticateWithFBToken = (fbToken: string): Auth | Object =>
+	client
+		.query({
+			query: gql`
+				query auth($token: String!) {
+					authenticateFB(facebookToken: $token) {
+						token
+						user
+					}
+				}
+			`,
+			fetchPolicy: 'network-only',
+			variables: { token: fbToken }
+		})
+		.then(response => graphResponse.data.authenticateFB)
+
+export const fetchUser = (id: string): ApolloUserResponse =>
+	client
+		.query({
+			query: gql`
+				query getUser($id: ID!) {
+					User(id: $id) {
+						id
+						facebookId
+						name
+						steps(orderBy: stepId_DESC, first: 1) {
+							id
+							stepId
+							data
+						}
+					}
+				}
+			`,
+			fetchPolicy: 'network-only',
+			variables: { id }
+		})
+		.then(response => graphResponse.data.User)
+
+export const addStep = (userId: string, stepId: string, formData: any) =>
+	client.mutate({
+		mutation: gql`
+			mutation add($userId: ID!, $stepInput: Json!) {
+				addStep(userId: $userId, stepInput: $stepInput) {
+					message
+					user
+				}
+			}
+		`,
+		variables: { userId: userId, stepInput: JSON.stringify({ stepId, data: formData }) }
+	})
