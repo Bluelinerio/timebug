cba4406961c3fdd5f389fda2cd02ce5bb343a154
Daniel Reinoso <danielreinoso187@gmail.com>
2017-12-21 23:09:25 -0300

    Remove hidden component as they create pages

--- src/screens/WorkBookScreen/components/templates/customStruct.js
+++ src/screens/WorkBookScreen/components/templates/customStruct.js
@@ -1,5 +1,5 @@
-var React = require('react');
-var { View, Text } = require('react-native');
+import React from 'react';
+import { View, Text } from 'react-native';
 import { Pages } from 'react-native-pages';
 
 function struct(locals) {
@@ -7,20 +7,24 @@ function struct(locals) {
     return null;
   }
 
-  var stylesheet = locals.stylesheet;
-  var fieldsetStyle = stylesheet.fieldset;
-  var controlLabelStyle = stylesheet.controlLabel.normal;
+  const stylesheet = locals.stylesheet;
+  const fieldsetStyle = stylesheet.fieldset;
+  let controlLabelStyle = stylesheet.controlLabel.normal;
 
   if (locals.hasError) {
     controlLabelStyle = stylesheet.controlLabel.error;
   }
 
-  var label = locals.label ? <Text style={controlLabelStyle}>{locals.label}</Text> : null;
-  var error = locals.hasError && locals.error ? <Text accessibilityLiveRegion="polite" style={stylesheet.errorBlock}>{locals.error}</Text> : null;
+  const label = locals.label ? <Text style={controlLabelStyle}>{locals.label}</Text> : null;
+  const error = locals.hasError && locals.error ? <Text accessibilityLiveRegion="polite" style={stylesheet.errorBlock}>{locals.error}</Text> : null;
 
-  var rows = locals.order.map(function (name) {
+  const rows = locals.order.map(function (name) {
     return locals.inputs[name];
   });
+
+  const filteredRows = rows.filter(input => {
+    return !input.props.options || (input.props.options && !input.props.options.hidden);
+  })
 
   return (
     <View style={{ flex: 1, backgroundColor: '#CCC' }}>
@@ -31,8 +35,8 @@ function struct(locals) {
           {rows}
         </View>
       ) : (
-        <Pages horizontal={false} containerStyle={{ padding: 20, justifyContent: 'center'}}>
-          {rows}
+        <Pages horizontal={false} containerStyle={{ padding: 20}}>
+          {filteredRows}
         </Pages>
       )}
     </View>
