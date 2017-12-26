f7158d4cd27b356cdb68a832ea6128f8ffd059dc
Amos Elmaliah <amosel@gmail.com>
2017-12-26 16:43:50 -0500

    Added BackHandler on top of ReduxNavigator.

--- src/navigation/reduxNavigator.js
+++ src/navigation/reduxNavigator.js
@@ -1,19 +1,36 @@
 import React                  from 'react';
 import * as ReactNavigation   from 'react-navigation';
+import { BackHandler }        from "react-native";
 import { connect }            from 'react-redux';
 import Navigator              from './navigator';
 import * as NavigationService from '../HOC/navigation'
 
 const mapStateToProps = state => ({ nav: state.nav });
 
-const ReduxNavigator = ({ dispatch, nav }) => {
-  const navigation = ReactNavigation.addNavigationHelpers({
-    dispatch,
-    state: nav,
-  });
-  NavigationService.setNavigator(navigation);
-
-  return <Navigator navigation={navigation} />;
+class ReduxNavigation extends React.Component {
+  componentDidMount() {
+    BackHandler.addEventListener("hardwareBackPress", this.onBackPress);
+  }
+  componentWillUnmount() {
+    BackHandler.removeEventListener("hardwareBackPress", this.onBackPress);
+  }
+  onBackPress = () => {
+    const { dispatch, nav } = this.props;
+    if (nav.index === 0) {
+      return false;
+    }
+    dispatch(NavigationActions.back());
+    return true;
+  }
+  render() {
+    const { dispatch, nav } = this.props;
+    const navigation = ReactNavigation.addNavigationHelpers({
+      dispatch,
+      state: nav
+    });
+    NavigationService.setNavigator(navigation);
+    return <Navigator navigation={navigation} />;
+  }
 }
 
-export default connect(mapStateToProps)(ReduxNavigator);
+export default connect(mapStateToProps)(ReduxNavigation);
