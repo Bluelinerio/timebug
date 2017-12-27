9f8fd7cc4d8c34d09efdf0596ec80c5c199d20fe
Amos Elmaliah <amosel@gmail.com>
2017-12-27 13:05:28 -0500

    Added pointer to where the static update script needs to be.

---
+++ scripts/updateStatic.js
@@ -0,0 +1,4 @@
+'use strict';
+// this idea of this is to update the content from the CMS, including the vactors once we have started using redux-offline.
+//const express = require('express');
+const refreshCMS = require('../src/services/contentful');
