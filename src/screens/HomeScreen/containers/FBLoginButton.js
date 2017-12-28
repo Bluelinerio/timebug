f1ff85765e96c7c14eb591e54e212d7ac5489a95
Amos Elmaliah <amosel@gmail.com>
2017-12-28 03:32:08 -0500

    clean up LogoutButton and FBLoginButton

---
+++ src/screens/HomeScreen/containers/FBLoginButton.js
@@ -0,0 +1,19 @@
+// @flow
+import React from 'react'
+import { connect } from 'react-redux'
+import Button from '../../../components/Button'
+import styles from '../../../styles/loginButton.styles'
+import { loginWithFbButtonPressed } from '../../../redux/actions'
+
+const FBLoginButton = ({ loginWithFbButtonPressed, ...rest }) => (
+	<Button
+		textTestId={'login_text'}
+		buttonTestId={'login_button'}
+		text={'LOGIN WITH FACEBOOK'}
+		styles={styles}
+		onPress={loginWithFbButtonPressed}
+		{...rest}
+	/>
+)
+
+export default connect(null, ({loginWithFbButtonPressed}) )(FBLoginButton)
--- src/screens/HomeScreen/containers/LogoutButton.js
+++ src/screens/HomeScreen/containers/LogoutButton.js
@@ -2,9 +2,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import Button from '../../../components/Button'
-import styles from '../styles/loginButton.styles'
+import styles from '../../../styles/loginButton.styles'
 import { logoutButtonPressed } from '../../../redux/actions'
-import LoginButtonStyles from '../../../styles/components/LoginButton'
 
 const LogoutButton = ({ logoutButtonPressed, ...rest }) => (
 	<Button
