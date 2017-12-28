dbb846960a1e9686e2cec5195bc8b4209ba4d81c
Amos Elmaliah <amosel@gmail.com>
2017-12-28 14:33:54 -0500

    added missing businessLogic.js file that was removed in previos merge

---
+++ src/screens/WorkBookScreen/forms/businessLogic.js
@@ -0,0 +1,63 @@
+import { businessLogic as step1BusinessLogic } from './step1';
+import { businessLogic as step2BusinessLogic } from './step2';
+import { businessLogic as step3BusinessLogic } from './step3';
+import { businessLogic as step4BusinessLogic } from './step4';
+import { businessLogic as step5BusinessLogic } from './step5';
+import { businessLogic as step6BusinessLogic } from './step6';
+import { businessLogic as step7BusinessLogic } from './step7';
+import { businessLogic as step8BusinessLogic } from './step8';
+import { businessLogic as step9BusinessLogic } from './step9';
+import { businessLogic as step10BusinessLogic } from './step10';
+import { businessLogic as step11BusinessLogic } from './step11';
+import { businessLogic as step12BusinessLogic } from './step12';
+import { businessLogic as step13BusinessLogic } from './step13';
+import { businessLogic as step14BusinessLogic } from './step14';
+import { businessLogic as step15BusinessLogic } from './step15';
+import { businessLogic as step16BusinessLogic } from './step16';
+import { businessLogic as step17BusinessLogic } from './step17';
+import { businessLogic as step18BusinessLogic } from './step18';
+import { businessLogic as step19BusinessLogic } from './step19';
+import { businessLogic as step20BusinessLogic } from './step20';
+import { businessLogic as step21BusinessLogic } from './step21';
+import { businessLogic as step22BusinessLogic } from './step22';
+import { businessLogic as step23BusinessLogic } from './step23';
+import { businessLogic as step24BusinessLogic } from './step24';
+import { businessLogic as step25BusinessLogic } from './step25';
+import { businessLogic as step26BusinessLogic } from './step26';
+import { businessLogic as step27BusinessLogic } from './step27';
+import { businessLogic as step28BusinessLogic } from './step28';
+import { businessLogic as step29BusinessLogic } from './step29';
+import { businessLogic as step30BusinessLogic } from './step30';
+
+export default {
+  1: step1BusinessLogic,
+  2: step2BusinessLogic,
+  3: step3BusinessLogic,
+  4: step4BusinessLogic,
+  5: step5BusinessLogic,
+  6: step6BusinessLogic,
+  7: step7BusinessLogic,
+  8: step8BusinessLogic,
+  9: step9BusinessLogic,
+  10: step10BusinessLogic,
+  11: step11BusinessLogic,
+  12: step12BusinessLogic,
+  13: step13BusinessLogic,
+  14: step14BusinessLogic,
+  15: step15BusinessLogic,
+  16: step16BusinessLogic,
+  17: step17BusinessLogic,
+  18: step18BusinessLogic,
+  19: step19BusinessLogic,
+  20: step20BusinessLogic,
+  21: step21BusinessLogic,
+  22: step22BusinessLogic,
+  23: step23BusinessLogic,
+  24: step24BusinessLogic,
+  25: step25BusinessLogic,
+  26: step26BusinessLogic,
+  27: step27BusinessLogic,
+  28: step28BusinessLogic,
+  29: step29BusinessLogic,
+  30: step30BusinessLogic
+}
