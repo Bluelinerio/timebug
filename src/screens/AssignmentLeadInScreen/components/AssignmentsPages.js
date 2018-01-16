0de9ad8d6b0aa3729c211fb8b50126aaba539f11
Amos Elmaliah <amosel@gmail.com>
2018-01-15 22:54:45 -0500

    no-feature changes to AssignmentPages wrapper component.(future use)

--- src/screens/AssignmentLeadInScreen/components/AssignmentsPages.js
+++ src/screens/AssignmentLeadInScreen/components/AssignmentsPages.js
@@ -4,6 +4,7 @@ import { View} from 'react-native';
 import { Pages } from 'react-native-pages';
 import type { Assignment, Step } from '../../../services/cms';
 import AssignmentLeadInPageComponent from './AssignmentLeadInPageComponent'
+import BeginWorkbookButton 	from '../containers/BeginWorkbookButton';
 
 export type Props = {
 	assignments: Array<Assignment>,
@@ -11,21 +12,40 @@ export type Props = {
   step: number
 };
 
-export default ({ assignments, color, step }: Props) => (
-  <View style={{ flex: 1 }}>
-    { assignments && 
-      <Pages horizontal={false}>
-        {assignments.map((assignment , index) => 
-          (<AssignmentLeadInPageComponent
-            key={index}
-            isLastItem={index === assignments.length - 1}
-            assignment={assignment}
-            color={color}
-            index={index}
-            step={step}
-          />))
+type State = {
+  index:number
+}
+export default class PageWrapper extends Component<Props, State> {
+  pages:Pages = null
+  state = {
+    index:0
+  }
+  render() {
+    debugger
+    const {	assignments, color, step } = this.props;
+    const { page } = this.state;
+    return (
+      <View style={{ flex: 1 }}>
+        {assignments && 
+        <Pages 
+          horizontal={false}
+          ref={ref => this.pages = ref }
+          onScrollEnd={index => this.setState({ index })}
+        >
+          {assignments.map((assignment , index) => 
+            (<AssignmentLeadInPageComponent
+              key={index}
+              isLastItem={index === assignments.length - 1}
+              assignment={assignment}
+              color={color}
+              index={index}
+              step={step}
+              scrollToPage={index => this.pages && this.pages.scrollToPage(index) }
+            />))
+          }
+        </Pages>
         }
-      </Pages>
-    }
-  </View>
-)
+    </View>
+    )
+  }
+}
