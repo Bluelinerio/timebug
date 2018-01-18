f3a91a3b8aff0c6b12a7d9c85da89846598393b2
Amos Elmaliah <amosel@gmail.com>
2018-01-17 19:42:58 -0500

    Added a stateReconciler to the cms  reducer.

--- src/redux/reducers/cms.reducer.js
+++ src/redux/reducers/cms.reducer.js
@@ -24,7 +24,7 @@ const initialState: CMSState = {
 	error: null
 }
 
-export default function(state: CMSState = initialState, action: StepsAction) {
+function cmsReducer(state: CMSState = initialState, action: StepsAction) {
 	switch (action.type) {
 		case FETCH_CMS.STARTED:
 			return { ...state, requestCount: state.requestCount + 1 }
@@ -41,3 +41,22 @@ export default function(state: CMSState = initialState, action: StepsAction) {
 			return state
 	}
 }
+
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+
+const persistConfig = {
+	key:'cms',
+	storage: storage,
+  blacklist: ['requestCount'],
+  stateReconciler : (inboundState: CMSState, originalState: CMSState, reducedState: CMSState) => {
+		const state = !inboundState || !inboundState.lastFetchDate || originalState.requestCount > 0
+				? originalState 
+			: originalState.lastFetchDate && originalState.lastFetchDate > inboundState.lastFetchDate
+				? originalState
+				: inboundState
+		return state
+	}
+};
+
+export default persistReducer(persistConfig, cmsReducer);
