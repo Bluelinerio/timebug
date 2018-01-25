d09ee1d066e81218763f6c05ddf96ad997875c65
Amos Elmaliah <amosel@gmail.com>
2018-01-25 16:19:38 -0500

    Added first new background image to the StepScreen.

--- src/resources/images/index.js
+++ src/resources/images/index.js
@@ -1,8 +1,9 @@
 const sandClockConfetti = require('./sandClockConfetti.png');
+const background = require('./background.png')
 
 export const startButtonBackgroundImage = sandClockConfetti;
 export const backgroundImage = sandClockConfetti;
 export const headerBackgroundImage = sandClockConfetti;
-export const headerBackground = sandClockConfetti;
+export const headerBackground = background;
 
 export const lifevisionCollage = require('./Lifevision-Collage.png')
