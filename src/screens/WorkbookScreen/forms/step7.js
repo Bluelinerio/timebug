d4e65f95084181397d3a80a1a3dc457d44f849b5
Amos Elmaliah <amosel@gmail.com>
2018-02-20 16:05:54 -0500

    Form: fix missing struct in form7 (and text formatting.)

--- src/screens/WorkbookScreen/forms/step7.js
+++ src/screens/WorkbookScreen/forms/step7.js
@@ -2,50 +2,53 @@ import t from "../components/templates";
 import { CommonGoalOutcomes } from "./contents";
 
 export default {
-    1: {
-        type: t.struct({
-          id:t.maybe(t.String),
-            field: t.list(
-                t.struct({
-                    goal: t.String,
-                    commonGoalOutcomes: CommonGoalOutcomes
-                 })
-              )
-        }),
-        options: {
-          fields: {
-            id: {
-              hidden: true
-            },
-            field: {
-              label: "What are 5 of your goals from the past 5 years?",              
-              auto:'placeholders',
-              disableOrder: true,
-              maxLines: 15,
-              config: {
-                maxLines: 15,
-              },
-              item: {
-               fields: {
-                 //goal:{error: 'Please enter a goal.'},
-                 commonGoalOutcomes:{label: 'Classify this goal according to the 7 Common Goal Outcomes listed below.'}
-               } 
-              }
-            },
+  1: {
+    type: t.struct({
+      id:t.maybe(t.String),
+        field: t.list(
+            t.struct({
+                goal: t.String,
+                commonGoalOutcomes: CommonGoalOutcomes
+              })
+          )
+    }),
+    options: {
+      fields: {
+        id: {
+          hidden: true
+        },
+        field: {
+          label: "What are 5 of your goals from the past 5 years?",              
+          auto:'placeholders',
+          disableOrder: true,
+          maxLines: 15,
+          config: {
+            maxLines: 15,
+          },
+          item: {
+            fields: {
+              //goal:{error: 'Please enter a goal.'},
+              commonGoalOutcomes:{label: 'Classify this goal according to the 7 Common Goal Outcomes listed below.'}
+            } 
           }
         },
-        value: {
-          fields: {
-            id:'step7+v0.0.0.1'
-          }
-        }
-      },
-    2: {
-        type:  t.String,
-        options: {
-              error: 'Please fill out this field.',
-              label:"Have you noticed any patterns? Have any guesses as to the reasons underlying such patterns?",
-              multiline:true
-            },
-          }  
+      }
+    },
+    value: {
+      fields: {
+        id:'step7+v0.0.0.1'
+      }
+    }
+  },
+  2: {
+    type: t.struct({
+      patterns: t.String
+    }),
+    options: {
+      fields: {
+        label:"Have you noticed any patterns? Have any guesses as to the reasons underlying such patterns?",
+        multiline: true
+      }
+    }
+  }  
 };
