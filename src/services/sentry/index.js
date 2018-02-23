3f5121a509a744ddffc188b50d21f6c03ff1f180
Amos Elmaliah <amosel@gmail.com>
2018-02-23 12:51:55 -0500

    fix typo.

--- src/services/sentry/index.js
+++ src/services/sentry/index.js
@@ -1,5 +1,5 @@
 import { Sentry } from 'react-native-sentry';
-import CodePush frZom "react-native-code-push";
+import CodePush from "react-native-code-push";
 
 Sentry.config('https://0de59c73057340d789df292efcbba25b:cbb27d7602214baa88ace02da408939a@sentry.io/293417').install();
 
