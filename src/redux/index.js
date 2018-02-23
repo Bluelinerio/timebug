564f3f0468a25d236f647bfe5908c91d002ba963
Amos Elmaliah <amosel@gmail.com>
2018-02-23 18:59:26 -0500

    react-navigation - use most recent version.

--- src/redux/index.js
+++ src/redux/index.js
@@ -7,6 +7,7 @@ import {
 }                                               from 'redux'
 import createSagaMiddleware                     from 'redux-saga'
 import { persistStore }                         from 'redux-persist'
+import { createReactNavigationReduxMiddleware } from 'react-navigation-redux-helpers';
 import { AsyncStorage }                         from 'react-native'
 import applyAppStateListener                    from 'redux-enhancer-react-native-appstate';
 import rootSaga                                 from './rootSagas'
@@ -20,6 +21,11 @@ type Props = {
 }
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const navigationMiddleware = createReactNavigationReduxMiddleware(
+  'root',
+  state => state.nav,
+);
+
 export default () => {
 
   const sagaMiddleware = createSagaMiddleware();
@@ -27,7 +33,7 @@ export default () => {
     composeEnhancers(
       applyAppStateListener(),
       applyMiddleware(sagaMiddleware),
-      applyMiddleware(reactNavigationMiddleware)
+      applyMiddleware(navigationMiddleware)
     )
   );
   const persistor = persistStore(store);
