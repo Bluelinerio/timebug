2b5118203ea4e689dcfec2185b9a1ae717510684
Amos Elmaliah <amosel@gmail.com>
2018-02-25 13:26:01 -0500

    Sentry will report all bugs including on simulator. Added Doc about that and how to avoid.

--- src/services/sentry/index.js
+++ src/services/sentry/index.js
@@ -1,11 +1,13 @@
 import { Sentry } from 'react-native-sentry';
 import CodePush from "react-native-code-push";
+import DeviceInfo from 'react-native-device-info';
 
-Sentry.config('https://0de59c73057340d789df292efcbba25b:cbb27d7602214baa88ace02da408939a@sentry.io/293417').install();
+if(!DeviceInfo.isEmulator) {
+  Sentry.config('https://0de59c73057340d789df292efcbba25b:cbb27d7602214baa88ace02da408939a@sentry.io/293417').install();
 
-CodePush.getUpdateMetadata().then((update) => {
-  if (update) {
-    Sentry.setVersion(update.appVersion + '-codepush:' + update.label);
-  }
-});
-
+  CodePush.getUpdateMetadata().then((update) => {
+    if (update) {
+      Sentry.setVersion(update.appVersion + '-codepush:' + update.label);
+    }
+  });
+}
