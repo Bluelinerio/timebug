7284bc801995b73e58e81f781cd088e6741f1b00
Amos Elmaliah <amosel@gmail.com>
2018-03-01 14:11:36 -0500

    re-format spacing

--- src/screens/WalkthroughScreen/Walkthrough.js
+++ src/screens/WalkthroughScreen/Walkthrough.js
@@ -1,118 +1,118 @@
 // @flow
 import * as React from 'react';
 import {
-  View, 
-  Image, 
-  StyleSheet, 
-  Dimensions, 
-  StatusBar, 
+  View,
+  Image,
+  StyleSheet,
+  Dimensions,
+  StatusBar,
   Platform
 } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
 import Swiper from 'react-native-swiper';
 import LinearGradient from 'react-native-linear-gradient'
 
-import type { Slide }       from '../../services/cms';
-import Theme                from './components/Theme'
-import Text                 from './components/Text'
-import Button               from './components/Button'
+import type { Slide } from '../../services/cms';
+import Theme from './components/Theme'
+import Text from './components/Text'
+import Button from './components/Button'
 import type { ScreenProps } from './components/Types';
 
 const height = Dimensions.get('window').height
 const width = Dimensions.get('window').width
 
 type Props = ScreenProps & {
-  slides:[Slide],
-  dismiss:() => void
+  slides: [Slide],
+  dismiss: () => void
 }
 
 export default class Walkthrough extends React.Component<Props> {
   onIndexChanged = (index: number) => {
-      //slides[index].makeVisible();
+    //slides[index].makeVisible();
   }
   renderSlide = (slide: Slide, index: number) => (
     <LinearGradient
       key={index}
-      colors={['#008EBC', '#005587']} 
+      colors={['#008EBC', '#005587']}
       style={StyleSheet.absoluteFillObject, {
         height, 
       }}
     >
-      <StatusBar 
-        translucent 
-        barStyle='light-content'
-        backgroundColor={'transparent'}
-      /> 
-      <SafeAreaView style={styles.fullHeightView} >
-        <View style={styles.slide} >
-          <Text 
-            type='header2' 
-            style={styles.title} 
-            theme={theme}
-          >
-            {slide.title}
-          </Text>
-          {slide.description && 
-            <Text 
-              type='header3' 
-              style={styles.description}
-              theme={theme}
-            >
-              {slide.description}
-            </Text>
-          }
-          </View>
-          <View style={styles.slideImage}>
-          {slide.image && 
-            <Image 
-            source={slide.image}
-            resizeMode='cover'
-            style={{width: (width * .60) , height: 410}}
-             />
-          }
-          </View>
-        
-      </SafeAreaView>
-    </LinearGradient>
+  <StatusBar
+    translucent
+    barStyle='light-content'
+    backgroundColor={'transparent'}
+  />
+  <SafeAreaView style={styles.fullHeightView} >
+    <View style={styles.slide} >
+      <Text
+        type='header2'
+        style={styles.title}
+        theme={theme}
+      >
+        {slide.title}
+      </Text>
+      {slide.description &&
+        <Text
+          type='header3'
+          style={styles.description}
+          theme={theme}
+        >
+          {slide.description}
+        </Text>
+      }
+    </View>
+    <View style={styles.slideImage}>
+      {slide.image &&
+        <Image
+          source={slide.image}
+          resizeMode='cover'
+          style={{ width: (width * .60), height: 410 }}
+        />
+      }
+    </View>
+
+  </SafeAreaView>
+    </LinearGradient >
   )
 
-  render(): React.Node {
-    const { renderPagination, onIndexChanged} = this;
-    const { slides } = this.props
-    return (
-      <Swiper loop={false} {...{ renderPagination, onIndexChanged }} >
-        { slides.map(this.renderSlide) }
-      </Swiper>
-    );
-  }
-  renderPagination = (index: number, total: number, context: Swiper): React.Node => {
-      const isFirst = index === 0;
-      const isLast = index === total - 1;
-      const { dismiss } = this.props
-      const goBack = () => context.scrollBy(-1);
-      const goForward = () => context.scrollBy(1);
-      const leftButtoOnPress = isFirst ? dismiss : goBack
-      const rightButtonOnPress = isLast ? dismiss : goForward
-      return (
-        <View style={styles.footer} >
-          <Button 
-            label={isFirst ? 'Close' : 'Back'} 
-            onPress={leftButtoOnPress} 
-          />
-          <Button 
-            label={isLast ? 'Start' : 'Next'} 
-            onPress={rightButtonOnPress} 
-            primary={true} 
-            transparent={true} 
-          />
-        </View>
-      );
-  }
+render(): React.Node {
+  const { renderPagination, onIndexChanged } = this;
+  const { slides } = this.props
+  return (
+    <Swiper loop={false} {...{ renderPagination, onIndexChanged }} >
+      {slides.map(this.renderSlide)}
+    </Swiper>
+  );
+}
+renderPagination = (index: number, total: number, context: Swiper): React.Node => {
+  const isFirst = index === 0;
+  const isLast = index === total - 1;
+  const { dismiss } = this.props
+  const goBack = () => context.scrollBy(-1);
+  const goForward = () => context.scrollBy(1);
+  const leftButtoOnPress = isFirst ? dismiss : goBack
+  const rightButtonOnPress = isLast ? dismiss : goForward
+  return (
+    <View style={styles.footer} >
+      <Button
+        label={isFirst ? 'Close' : 'Back'}
+        onPress={leftButtoOnPress}
+      />
+      <Button
+        label={isLast ? 'Start' : 'Next'}
+        onPress={rightButtonOnPress}
+        primary={true}
+        transparent={true}
+      />
+    </View>
+  );
+}
 }
 
 
 const baseSpacing = Math.floor(width * 0.06)
-const largeVerticalSpacing  = Math.floor(height * 0.06)
+const largeVerticalSpacing = Math.floor(height * 0.06)
 
 const styles = StyleSheet.create({
   container: {
@@ -130,36 +130,36 @@ const styles = StyleSheet.create({
   },
   slideImage: {
     justifyContent: 'center',
-    alignItems:'center',
-    position:'absolute',
-    bottom:0,
-    left:0,
-    right:0,
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
     ...Platform.select({
-			android: { 
+      android: {
         paddingHorizontal: 10,
         borderRadius: 10,
         marginVertical: 0, // needed for shadow
         elevation: 10,
         width: (width * .60),
-        marginLeft: (width * .20) 
-          },
-			ios: {
-				shadowColor: "black",
-				shadowOpacity: 0.5
-			}
-		})
+        marginLeft: (width * .20)
+      },
+      ios: {
+        shadowColor: "black",
+        shadowOpacity: 0.5
+      }
+    })
   },
 
   fullHeightView: {
-    height: Dimensions.get('window').height   
+    height: Dimensions.get('window').height
   },
   title: {
     marginTop: largeVerticalSpacing,
     color: 'white'
   },
   description: {
-    marginTop:largeVerticalSpacing,
+    marginTop: largeVerticalSpacing,
     color: 'white'
   }
 })
