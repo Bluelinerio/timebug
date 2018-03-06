c50e76485e62067e107b6d412404b15abbbcc8b5
Amos Elmaliah <amosel@gmail.com>
2018-03-06 18:31:44 -0500

    Do not show Dashoboard for now.

--- src/screens/HomeScreen/containers/DashboardCellContainer/index.js
+++ src/screens/HomeScreen/containers/DashboardCellContainer/index.js
@@ -2,12 +2,24 @@ import * as React from 'react'
 import {
   View
 } from 'react-native'
+import User 															from '../../../../containers/User';
 import LifevisionDashoboardCellContainer	from './LifevisionDashoboardCellContainer';
 import MeditationDashobardCellContainer		from './MeditationDashobardCellContainer';
 
+const renderWithUser = (user) => {
+	if(user.showDashboard) {
+		return (
+			<View style={{ flex: 1}} >
+				<LifevisionDashoboardCellContainer />
+				<MeditationDashobardCellContainer />
+			</View>
+		)
+	}
+	return null
+}
+
 export default DashboardCellsContainer = () => (
-	<View style={{flex:1}} >
-		<LifevisionDashoboardCellContainer />
-		<MeditationDashobardCellContainer />
-	</View >
+	<User 
+		renderWithUser={renderWithUser}
+	/>
 )
