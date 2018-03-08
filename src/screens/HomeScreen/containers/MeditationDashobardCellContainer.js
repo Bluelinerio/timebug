ff7b3ede30beec7f3f7814edf4fd0bf4a3439eb2
Amos Elmaliah <amosel@gmail.com>
2018-03-08 11:40:29 -0500

    Removed unuse file.

--- src/screens/HomeScreen/containers/MeditationDashobardCellContainer.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { connect } from 'react-redux';
-import { goToMeditation } from '../../../redux/actions/nav.actions';
-import MeditationDashobardCell from '../components/DashboardCells/MeditationDashbardCell';
-
-export default connect(null, { onPress: goToMeditation })(
-  MeditationDashobardCell
-);
