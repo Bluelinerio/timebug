056d6c67d636711ef9e7d697f00b6e50f46de8fd
Amos Elmaliah <amosel@gmail.com>
2018-03-08 12:48:44 -0500

    Workbook Screen Container: refactor.

--- src/screens/WorkbookScreen/containers/WorkbookScreenContainer.js
+++ src/screens/WorkbookScreen/containers/WorkbookScreenContainer.js
@@ -1,52 +1,58 @@
 // @flow
 
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { NavigationActions, withNavigation } from 'react-navigation';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { NavigationActions, withNavigation } from 'react-navigation'
+import invariant from 'invariant'
 
-import { headerBackgrounds } from '../../../resources/images';
+import { headerBackgrounds }   from '../../../resources/images'
 import {
   submitFormValue,
   syncFormData
-} from '../../../redux/actions/formData.actions';
+}                              from '../../../redux/actions/formData.actions'
 import {
   goToWorkbookDoneScreen,
   goToWorkbookScreenWithParams
-} from '../../../redux/actions/nav.actions';
-import selectors from '../../../redux/selectors';
-import type Props from '../components/WorkbookScreenComponent';
-import WorkbookScreenComponent from '../components/WorkbookScreenComponent';
-import DefaultUserContainer from '../../../containers/DefaultUserContainer';
+}                              from '../../../redux/actions/nav.actions'
+import selectors               from '../../../redux/selectors'
+import type Props              from '../components/WorkbookScreenComponent'
+import WorkbookScreenComponent from '../components/WorkbookScreenComponent'
+import DefaultUserContainer    from '../../../containers/DefaultUserContainer'
 
 const mapStateToProps = state => {
-  const steps = selectors.steps(state);
-  const fetching = selectors.isSynchingFormData(state);
-  const modelsAndDataForExercise = selectors.modelsAndDataForExercise(state);
-  return { steps, fetching, modelsAndDataForExercise };
-};
+  const steps = selectors.steps(state)
+  const fetching = selectors.isSynchingFormData(state)
+  const modelsAndDataForExercise = selectors.modelsAndDataForExercise(state)
+  return { steps, fetching, modelsAndDataForExercise }
+}
 
 const merge = (stateProps, dispatchProps, ownProps): Props => {
-  const { colors, steps } = stateProps;
-  const { navigation: { state: { params } } } = ownProps;
-  const { stepId, formId, stepColor } = params;
+  const { colors, steps } = stateProps
+  const { navigation: { state: { params } } } = ownProps
+  const { stepId, formId, stepColor } = params
+  
+  invariant(invariant, 'workbook container expected params in naviation')
+  invariant(stepId && formId && stepColor, 'workbook container expected stepId && formId && stepColor in params in naviation')
+  
+  const { models, formData } = stateProps.modelsAndDataForExercise(stepId)
 
-  const { models, formData } = stateProps.modelsAndDataForExercise(stepId);
-  if (Object.keys(models).includes(formId) === false) {
-    throw `did not find model for formId ${formId} for stepId:${stepId}`;
-  }
+  invariant(
+    Object.keys(models).includes(formId), 
+    `did not find model for formId ${formId} for stepId:${stepId}`
+  )
 
-  const numberOfForms = Object.entries(models).length;
-  const formIdIndex = Object.keys(models).indexOf(formId);
-  const isFinalForm = numberOfForms - 1 === formIdIndex;
-  const ifFirstForm = formIdIndex === 0;
-  const buttonMessage = isFinalForm ? 'SUBMIT' : 'NEXT';
+  const numberOfForms = Object.keys(models).length
+  const formIdIndex = Object.keys(models).indexOf(formId)
+  const isFinalForm = numberOfForms - 1 === formIdIndex
+  const ifFirstForm = formIdIndex === 0
+  const buttonMessage = isFinalForm ? 'SUBMIT' : 'NEXT'
 
-  const model = models[formId];
-  const value = formData[formId];
+  const model = models[formId]
+  const value = formData[formId]
 
-  const isFetching = !models ? true : stateProps.network > 0;
+  const isFetching = !models ? true : stateProps.network > 0
 
-  const backgroundImage = headerBackgrounds[stepId];
+  const backgroundImage = headerBackgrounds[stepId]
 
   const nextActions = isFinalForm
     ? [syncFormData(), goToWorkbookDoneScreen(ownProps)]
@@ -55,10 +61,10 @@ const merge = (stateProps, dispatchProps, ownProps): Props => {
           ...params,
           formId: Object.keys(models)[formIdIndex + 1]
         })
-      ];
+      ]
 
   const next = () =>
-    nextActions.forEach(action => ownProps.navigation.dispatch(action));
+    nextActions.forEach(action => ownProps.navigation.dispatch(action))
 
   const submit = value =>
     ownProps.navigation.dispatch(
@@ -67,7 +73,7 @@ const merge = (stateProps, dispatchProps, ownProps): Props => {
         stepId,
         value
       })
-    );
+    )
 
   return {
     ...ownProps,
@@ -82,12 +88,12 @@ const merge = (stateProps, dispatchProps, ownProps): Props => {
     isFetching,
     submit,
     backgroundImage
-  };
-};
+  }
+}
 
 const WorkbookScreenContainer = withNavigation(
   connect(mapStateToProps, null, merge)(WorkbookScreenComponent)
-);
+)
 
 export default () => (
   <DefaultUserContainer
@@ -96,4 +102,4 @@ export default () => (
       'You need to be logged in to be able to do the exercises. Please go back and log in again.'
     }
   />
-);
+)
