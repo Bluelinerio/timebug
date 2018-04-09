50bccf962c10865f82b2708833faa74eb17fb7b4
Amos Elmaliah <amosel@gmail.com>
2018-04-09 16:29:10 -0400

    Added loading button to the StepScreenButtonContainer

--- src/screens/StepScreen/containers/StepScreenButtonContainer.js
+++ src/screens/StepScreen/containers/StepScreenButtonContainer.js
@@ -1,16 +1,29 @@
-import * as React from 'react';
-import Button from '../../../components/Button';
-import User from '../../../containers/User';
-import LoginWithFbButtonContainer from '../../../containers/LoginWithFbButtonContainer';
-import BeginExerciseButtonContainer from './BeginExerciseButtonContainer';
+//@flow
+import * as React from 'react'
+import { View, ActivityIndicator } from 'react-native'
+import User from '../../../containers/User'
+import LoginWithFbButtonContainer from '../../../containers/LoginWithFbButtonContainer'
+import BeginExerciseButtonContainer from './BeginExerciseButtonContainer'
 
-export default () => (
+const LoadingIndicator = () => (
+  <View
+    style={{
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}
+  >
+    <ActivityIndicator />
+  </View>
+)
+
+const StepScreenButtonContainer = () => (
   <User
     renderWithUser={() => <BeginExerciseButtonContainer />}
-    renderWithAuthenticating={() => (
-      <Button backgroundColor="white" text="Loading..." disabled={true} />
-    )}
-    renderWithUndetermined={() => null}
+    renderWithAuthenticating={() => <LoadingIndicator />}
+    renderWithUndetermined={() => <LoadingIndicator />}
     renderWithAnonymous={() => <LoginWithFbButtonContainer />}
   />
-);
+)
+
+export default StepScreenButtonContainer
