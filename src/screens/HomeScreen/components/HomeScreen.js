466ef2bc6f3bd548d63f9728eb5111141024e4e2
Amos Elmaliah <amosel@gmail.com>
2018-04-09 16:35:09 -0400

    HomeScreen: Made Android Status bar white (instead of transparent)

--- src/screens/HomeScreen/components/HomeScreen.js
+++ src/screens/HomeScreen/components/HomeScreen.js
@@ -3,35 +3,18 @@ import React, { PureComponent } from 'react'
 import { 
 	StatusBar,
 	View,
-	Text, 
 	ScrollView, 
-	TouchableHighlight, 
 } from 'react-native'
 import { SafeAreaView } from 'react-navigation'
 
-import GradientWithTwoColors							from '../../../components/GradientWithTwoColors'
 import PagninatedCarouselContainer  			from '../containers/PagninatedCarouselContainer'
 import Version														from '../containers/Version'
 import DashboardCellsContainer						from '../containers/DashboardCellContainer'
 import MoreButtonContainer								from '../containers/MoreButtonContainer'
 import styles	           									from '../styles'
 import { sliderWidth, itemWidth }   			from '../styles/SliderEntry.style'
-import PagninatedCarousel           			from './PagninatedCarousel'
 import Banner                       			from './Banner'
 import Space															from './Space'
-/// end other.
-
-const StatusBarHeightSlugForAndroid = ({ backgroundColor = 'white' }) => {
-	if( StatusBar.currentHeight && StatusBar.translucent ) {
-		return <View 
-							style={{
-								backgroundColor,
-								height: StatusBar.currentHeight
-							}} 
-            />
-	}
-	return null
-}
 
 export default class HomeScreen extends PureComponent {
 	render() {
@@ -41,15 +24,13 @@ export default class HomeScreen extends PureComponent {
 				style={styles.container}
 			>
 				<StatusBar 
-					translucent
 					barStyle='dark-content'
-					backgroundColor={'transparent'}
+					backgroundColor={'white'}
 				/> 
 				<ScrollView 
 					style={{ flex: 1 }} 
 					contentInsetAdjustmentBehavior="automatic"
 				>	
-					<StatusBarHeightSlugForAndroid backgroundColor={'white'}/>
 					<Banner />
 					<PagninatedCarouselContainer
 						itemWidth={itemWidth}
