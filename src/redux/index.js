e8ce8eeb6c5d4312e0ee7ee705aaffc02a12abcb
Amos Elmaliah <amosel@gmail.com>
2018-04-11 12:49:23 -0400

    yarn add redux-thunk + implement.

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,39 +1,40 @@
 // @flow
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import createSagaMiddleware from 'redux-saga';
-import { persistStore } from 'redux-persist';
-import { createReactNavigationReduxMiddleware } from 'react-navigation-redux-helpers';
-import applyAppStateListener from 'redux-enhancer-react-native-appstate';
-import reduxReset from 'redux-reset';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import thunk from 'redux-thunk'
+import { persistStore } from 'redux-persist'
+import { createReactNavigationReduxMiddleware } from 'react-navigation-redux-helpers'
+import applyAppStateListener from 'redux-enhancer-react-native-appstate'
+import reduxReset from 'redux-reset'
 
-import rootSaga from './rootSagas';
-import { rootReducer } from './rootReducer';
-import { reactNavigationMiddleware } from './middlewares';
-import { resetStore } from './actions';
+import rootSaga from './rootSagas'
+import { rootReducer } from './rootReducer'
+import { reactNavigationMiddleware } from './middlewares'
+import { resetStore } from './actions'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const navigationMiddleware = createReactNavigationReduxMiddleware(
   'root',
   state => state.nav
-);
+)
 
 export default () => {
-  const sagaMiddleware = createSagaMiddleware();
+  const sagaMiddleware = createSagaMiddleware()
   const store = createStore(
     combineReducers(rootReducer),
     composeEnhancers(
       reduxReset(resetStore.type), // Set action.type here
       applyAppStateListener(),
-      applyMiddleware(sagaMiddleware),
+      applyMiddleware(thunk, sagaMiddleware),
       applyMiddleware(reactNavigationMiddleware)
     )
-  );
-  const persistor = persistStore(store);
+  )
+  const persistor = persistStore(store)
 
-  sagaMiddleware.run(rootSaga);
+  sagaMiddleware.run(rootSaga)
 
   return {
     persistor,
     store
-  };
-};
+  }
+}
