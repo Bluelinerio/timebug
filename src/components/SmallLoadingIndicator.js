bf5c56f0fefae9186511e9da622b14a8c019f4e2
Amos Elmaliah <amosel@gmail.com>
2018-04-12 14:44:31 -0400

    StepScreen: Small Loading View.

---
+++ src/components/SmallLoadingIndicator.js
@@ -0,0 +1,26 @@
+//@flow
+import * as React from 'react'
+import { View, Text, ActivityIndicator } from 'react-native'
+
+const SmallLoadingIndicator = ({ message = '' }: { message: ?string }) => (
+  <View
+    style={{
+      flex: 1,
+      marginTop: 20,
+      marginBottom: 20,
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}
+  >
+    <Text
+      style={{
+        marginBottom: 5
+      }}
+    >
+      {message}
+    </Text>
+    <ActivityIndicator />
+  </View>
+)
+
+export default SmallLoadingIndicator
--- src/screens/StepScreen/containers/StepScreenButtonContainer.js
+++ src/screens/StepScreen/containers/StepScreenButtonContainer.js
@@ -1,27 +1,17 @@
 //@flow
 import * as React from 'react'
-import { View, ActivityIndicator } from 'react-native'
 import User from '../../../containers/User'
 import LoginWithFbButtonContainer from '../../../containers/LoginWithFbButtonContainer'
 import BeginExerciseButtonContainer from './BeginExerciseButtonContainer'
-
-const LoadingIndicator = () => (
-  <View
-    style={{
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center'
-    }}
-  >
-    <ActivityIndicator />
-  </View>
-)
+import SmallLoadingIndicator from '../../../components/SmallLoadingIndicator'
 
 const StepScreenButtonContainer = () => (
   <User
     renderWithUser={() => <BeginExerciseButtonContainer />}
-    renderWithAuthenticating={() => <LoadingIndicator />}
-    renderWithUndetermined={() => <LoadingIndicator />}
+    renderWithAuthenticating={() => (
+      <SmallLoadingIndicator message={'Authenticating'} />
+    )}
+    renderWithUndetermined={() => <SmallLoadingIndicator />}
     renderWithAnonymous={() => <LoginWithFbButtonContainer />}
   />
 )
