7555cbba18240a9f773cbc09801087ba9469ce3a
Amos Elmaliah <amosel@gmail.com>
2018-04-12 18:01:16 -0400

    Added codepush version.

--- src/screens/HomeScreen/containers/Version.js
+++ src/screens/HomeScreen/containers/Version.js
@@ -1,18 +1,33 @@
-import React                                          from 'react'
-import { Text }                                       from 'react-native'
-import DeviceInfo                                     from 'react-native-device-info'
+import React from 'react'
+import { Text } from 'react-native'
+import DeviceInfo from 'react-native-device-info'
+import CodePush from 'react-native-code-push'
+//import version from 'react-native-version'
 
-const VersionComponent = () => (
-  <Text
-    style={{
-      textAlign: 'center',
-      fontSize: 10,
-      color: '#ccc',
-      marginBottom: 2
-    }}
-  >
-    {`20/20 Lifevision © blabsventures 2018\nVersion:${DeviceInfo.getVersion()} build:${DeviceInfo.getBuildNumber()} `}
-  </Text>
-)
+class VersionComponent extends React.Component {
+  state = { version: __DEV__ ? 'debug' : 'release' }
+  componentDidMount = () =>
+    CodePush.getUpdateMetadata().then(update => {
+      if (update) {
+        this.setState({ version: update.appVersion, codepush: update.label })
+      }
+    })
+  render() {
+    return (
+      <Text
+        style={{
+          textAlign: 'center',
+          fontSize: 10,
+          color: '#ccc',
+          marginBottom: 2
+        }}
+      >
+        {`20/20 Lifevision © blabsventures 2018\n Version:${
+          this.state.codepush
+        }, Native:${DeviceInfo.getVersion()} build:${DeviceInfo.getBuildNumber()} `}
+      </Text>
+    )
+  }
+}
 
 export default VersionComponent
