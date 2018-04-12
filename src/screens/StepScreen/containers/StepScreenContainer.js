fa7b729ca24bd54565ca58cb10be744dea6ffe23
Amos Elmaliah <amosel@gmail.com>
2018-04-12 16:06:40 -0400

    StepScreenContainer rewrittedn with recompose.

--- src/screens/StepScreen/containers/StepScreenContainer.js
+++ src/screens/StepScreen/containers/StepScreenContainer.js
@@ -1,60 +1,31 @@
 // @flow
+import StepScreenComponent from '../components/StepScreenComponent'
 
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { withNavigation } from 'react-navigation';
+import { connect } from 'react-redux'
+import { withNavigation } from 'react-navigation'
+import { compose, mapProps } from 'recompose'
+import selectors from '../../../redux/selectors'
 
-import StepScreen from '../components/StepScreen';
-import type Props from '../components/StepScreen';
-import { headerBackgrounds } from '../../../resources/images';
-import { getImageUrl } from '../../../services/cms';
-import selectors from '../../../redux/selectors';
-import type { Step } from '../../../services/cms';
-
-const mapStateToProps = state => {
-  const steps = selectors.steps(state);
-  return { steps };
-};
-
-const testNavigation = ownProps => {
-  if (!ownProps.navigation) throw 'did not find navigation';
-};
-const testStepId = ({ navigation: { state: { params: { stepId } } } }) => {
-  if (!stepId) {
-    throw `did not find stepId in navigation.state.params`;
-  }
-};
-
-const merge = (stateProps, dispatchProps, ownProps): Props => {
-  const { steps } = stateProps;
-  testNavigation(ownProps);
-  testStepId(ownProps);
-  const { navigation: { state: { params: { stepId } } } } = ownProps;
-  const step = steps[stepId];
-  if (!step) {
-    throw `did not find step with number ${stepId} in steps: ${steps}`;
-  }
-  const title = step.title;
-  const subtitle = `Step ${step.number}`;
-  const description = step.description;
-  const number = step.number;
-  const icon = step.icon;
-  const color = step.color;
-  const image = headerBackgrounds[stepId];
-
+const mapStepToStepScreenProps = step => {
   return {
-    title,
-    subtitle,
-    description,
-    number,
-    icon,
-    color,
-    image
-  };
-};
+    title: step.title,
+    subtitle: `Step ${step.number}`,
+    content: step.description,
+    color: step.color,
+    image: step.image
+  }
+}
 
-const StepScreenContainer = withNavigation(
-  connect(mapStateToProps, null , merge)(StepScreen)
-);
+const mapStepNavigationParam = ({
+  steps,
+  navigation: { state: { params: { stepId } } }
+}) => steps[stepId]
 
-export default StepScreenContainer;
+const StepScreenContainer = compose(
+  withNavigation,
+  connect(state => ({ steps: selectors.steps(state) })),
+  mapProps(mapStepNavigationParam),
+  mapProps(mapStepToStepScreenProps)
+)(StepScreenComponent)
+
+export default StepScreenContainer
