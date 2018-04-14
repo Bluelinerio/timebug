654646f9b66b0a6ac8f773d0f863455e2a6e1f44
Amos Elmaliah <amosel@gmail.com>
2018-04-14 19:10:40 -0400

    user.reducer: format.

--- src/redux/reducers/user.reducer.js
+++ src/redux/reducers/user.reducer.js
@@ -1,42 +1,42 @@
 // @flow
-import type { UserStateAction } from '../actions';
-import { SET_USER_STATE } from '../actions';
-import type { User, UserState } from '../../services/apollo/models';
+import type { UserStateAction }      from '../actions'
+import { SET_USER_STATE }            from '../actions'
+import type { User, UserState }      from '../../services/apollo/models'
 import {
   ANONYMOUS,
   UNDETERMINED,
   AUTHENTICATING
-} from '../../services/apollo/models';
-import { GET_USER, AUTHENTICATE_FB } from '../actions/user.actions';
+}                                    from '../../services/apollo/models'
+import { GET_USER, AUTHENTICATE_FB } from '../actions/user.actions'
 import {
   LOGOUT,
   LOGIN_WITH_FB_BUTTON_PRESSED,
   UPDATE_USER
-} from '../actionTypes';
+}                                    from '../actionTypes'
 
-export const userFromResponse = (response: any): User => response.data.User;
+export const userFromResponse = (response: any): User => response.data.User
 
-type LogoutAction = { type: LOGOUT };
+type LogoutAction = { type: LOGOUT }
 
-type Action = UserStateAction | LogoutAction;
+type Action = UserStateAction | LogoutAction
 
 export default function(state: UserState = UNDETERMINED, action: Action) {
   switch (action.type) {
     case LOGIN_WITH_FB_BUTTON_PRESSED:
     case AUTHENTICATE_FB.STARTED:
     case GET_USER.STARTED:
-      return AUTHENTICATING;
+      return AUTHENTICATING
     case LOGOUT:
-      return ANONYMOUS;
+      return ANONYMOUS
     case SET_USER_STATE:
     case GET_USER.SUCCEEDED:
-      return action.payload;
+      return action.payload
     case UPDATE_USER:
       return {
         ...state,
         ...action.payload
-      };
+      }
     default:
-      return state;
+      return state
   }
 }
