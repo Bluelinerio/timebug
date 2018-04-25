8b1885925ebb3c3d0cfb44bc713252efa85f5065
Amos Elmaliah <amosel@gmail.com>
2018-04-25 13:55:33 -0400

    WIP

--- src/screens/StepScreen/containers/BeginExerciseButtonContainer.js
+++ src/screens/StepScreen/containers/BeginExerciseButtonContainer.js
@@ -1,16 +1,47 @@
 import * as React from 'react'
 import { compose, withProps, mapProps } from 'recompose'
+import { connect } from 'react-redux'
 import Button from '../../../components/Button'
 import type { Props } from '../../../components/Button'
 import { goToWorkbookScreen } from '../../../redux/actions/nav.actions'
+import selectors from '../../../redux/selectors'
 import { withNavigationAndStep } from '../../../HOC'
+
+const mapStateToProps = (state: any) => {
+  const { sortedStepsWithForms } = selectors.sortedStepsWithForms(state)
+  return {
+    sortedStepsWithForms
+  }
+}
 
 const textTestId = 'step_to_workbook_text'
 const buttonTestId = 'step_to_workbook_button'
 const text = 'BEGIN'
 
-const mapToButton = ({ step, dispatch, ...rest }) : Props => ({
+const titleForButtonWithCompleteAndIncompleteForms = ({
+  completedForms,
+  incomplete,
+}) => {
+  Object.keys(incomplete).length > 0 ? 'Resume' : 'Edit'
+  if (Object.keys(incomplete).length > 0) {
+    return 'Resume'
+  } else if (completedForms && completedForms.length > 0) {
+    return 'Edit'
+  } else {
+    return 'Start'
+  }
+}
+
+const mapToButton = ({
+  step,
+  sortedStepsWithForms,
+  dispatch,
+  ...rest
+}): Props => ({
   ...rest,
+  text: titleForButtonWithCompleteAndIncompleteForms(
+    sortedStepsWithForms.find(s => s.step.stepId === step.stepId)
+  ),
   onPressWithProps: props => dispatch(goToWorkbookScreen(props)),
   backgroundColor: step.color
 })
@@ -22,6 +53,7 @@ const BeginExerciseButtonContainer = compose(
     text
   }),
   withNavigationAndStep,
+  connect(mapStateToProps),
   mapProps(mapToButton)
 )(Button)
 
