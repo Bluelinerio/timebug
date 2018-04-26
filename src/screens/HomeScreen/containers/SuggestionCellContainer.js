4454937e13c1b6291913b16addf8a147e279f7e4
Amos Elmaliah <amosel@gmail.com>
2018-04-26 14:06:47 -0400

    Suggestions Cell Container now uses a rules to determin which suggestions to show.

--- src/screens/HomeScreen/containers/SuggestionCellContainer.js
+++ src/screens/HomeScreen/containers/SuggestionCellContainer.js
@@ -1,18 +1,33 @@
 import * as React from 'react'
+import { connect } from 'react-redux'
 import { randomItem } from '../../../utils/random'
 import CheckinExerciseCellContainer from './CheckinExerciseCellContainer'
 import HowAreYouFeelingSuggestionCellContainer from './HowAreYouFeelingSuggestionCellContainer'
 import WhereToStartSuggestionCellContainer from './WhereToStartSuggestionCellContainer'
 import SwipablyDiscardableRow from '../../../components/SwipablyDiscardableRow'
+import { lastStepGuideVisited } from '../../../redux/selectors/agregates'
 
-const SuggestionCellContainer = props => (
-  <SwipablyDiscardableRow onClose={props.onClose}>
-    {randomItem([
-      <CheckinExerciseCellContainer {...props} />,
-      <HowAreYouFeelingSuggestionCellContainer {...props} />,
-      <WhereToStartSuggestionCellContainer {...props} />
-    ])}
-  </SwipablyDiscardableRow>
-)
+const pickSuggestion = state => ({
+  lastStepGuideVisited: lastStepGuideVisited(state)
+})
+
+const SuggestionCellPicker = (props: {
+  lastStepGuideVisited: {},
+  onClose: () => void
+}) => {
+  return (
+    <SwipablyDiscardableRow onClose={props.onClose}>
+      {!props.lastStepGuideVisited ? (
+        <WhereToStartSuggestionCellContainer {...props} />
+      ) : (
+        randomItem([
+          <CheckinExerciseCellContainer {...props} />,
+          <HowAreYouFeelingSuggestionCellContainer {...props} />
+        ])
+      )}
+    </SwipablyDiscardableRow>
+  )
+}
+const SuggestionCellContainer = connect(pickSuggestion)(SuggestionCellPicker)
 
 export default SuggestionCellContainer
