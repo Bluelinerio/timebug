5104ba2e5dbec53621127c142ddec1bd2cd0a352
Amos Elmaliah <amosel@gmail.com>
2018-04-26 09:36:15 -0400

    Agregate resucer, as part of research added a few events. not tested/ready for production yet .

--- src/redux/reducers/agregates.reducer.js
+++ src/redux/reducers/agregates.reducer.js
@@ -5,39 +5,60 @@
 
 // @flow
 
-export const UNDETERMIND = 0;
-export const UPDATE = 'UPDATE';
+export const UNDETERMIND = 0
+export const UPDATE = 'UPDATE'
+export const PUSH = 'PUSH'
+export const EVENT = 'EVENT'
 
-export type AggregateType = {};
+export type AggregateType = {}
 export type AggregateContainer = {
   +aggregate: AggregateType,
   +lastFetchDate: 0,
   +requestCount: 0,
   +error: null
-};
+}
 
-export type AggregateState = UNDETERMIND | AggregateContainer;
-export type AggregateUpdateAction = { type: UPDATE, payload: AggregateType };
+export type AggregateState = UNDETERMIND | AggregateContainer
+export type AggregateUpdateAction = { type: UPDATE, payload: AggregateType }
 
-export const initialState = UNDETERMIND;
+export const initialState = UNDETERMIND
 
+// re: react-navigation and redux-beacon review https://github.com/rangle/redux-beacon/issues/138 
 function aggregateReducer(
   state: AggregateState = initialState,
   action: AggregateUpdateAction
 ) {
   switch (action.type) {
+    case EVENT:
+      return {
+        ...(state && {}),
+        events: {
+          ...state.events,
+          [Date.now()]: action.payload
+        }
+      }
     case UPDATE:
       return {
         ...(state && {}),
         ...action.payload
-      };
+      }
+    case PUSH:
+      return {
+        ...(state && {}),
+        ...Object.keys(action.payload).reduce(
+          (sum, key) => ({
+            [key]: [...state[key], action.payload]
+          }),
+          {}
+        )
+      }
     default:
-      return state;
+      return state
   }
 }
 
-import storage from 'redux-persist/lib/storage';
-import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
+import { persistReducer } from 'redux-persist'
 
 const persistConfig = {
   key: 'aggregate',
@@ -49,7 +70,7 @@ const persistConfig = {
     reducedState: AggregateState
   ) => {
     if (inboundState === UNDETERMIND) {
-      return UNDETERMIND;
+      return UNDETERMIND
     }
     const state =
       !inboundState ||
@@ -59,9 +80,9 @@ const persistConfig = {
         : originalState.lastFetchDate &&
           originalState.lastFetchDate > inboundState.lastFetchDate
           ? originalState
-          : inboundState;
-    return state;
+          : inboundState
+    return state
   }
-};
+}
 
-export default persistReducer(persistConfig, aggregateReducer);
+export default persistReducer(persistConfig, aggregateReducer)
