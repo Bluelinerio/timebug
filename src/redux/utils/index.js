3d5b6e6c5016391d6356278f9a9718c2e628550b
Amos Elmaliah <amosel@gmail.com>
2018-07-07 13:28:42 -0400

    redux:util remove unused.

--- src/redux/utils/index.js
+++ src/redux/utils/index.js
@@ -3,20 +3,3 @@ export const action = (type: string, payload: any = {}) => ({
   type,
   ...payload
 });
-
-export type RunnableActionType = {
-  action: () => { type: string },
-  type: string
-};
-
-export const runnableAction = (type: string): RunnableActionType => ({
-  action: () => action(type),
-  type
-});
-
-export const createActionsObject = (base, ...types) => {
-  return types.reduce((acc, type) => {
-    acc[type] = `${base}_${type}`;
-    return acc;
-  }, {});
-};
