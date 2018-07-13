a165a3d652cd75c6f2344cccd16427ec2a059495
Graren <rennycol006@gmail.com>
2018-07-12 21:14:35 -0500

    WIP: InsightComponent Styled

--- src/screens/HomeScreen/components/InsightComponent.js
+++ src/screens/HomeScreen/components/InsightComponent.js
@@ -1,23 +1,45 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import dashboardStyles from '../../styles/dashboard.styles'
+import {
+    human,
+    systemWeights,
+    iOSColors,
+    iOSUIKit,
+    material,
+    sanFranciscoWeights,
+    robotoWeights
+} from 'react-native-typography'
 
 const styles = {
     suggestionText: {
         ...StyleSheet.flatten(dashboardStyles.suggestionText),
-        color: '#0098AA'
+        color: '#6AC2ED',
+        fontSize: 16,
+        lineHeight: 20        
     },
+    view: {
+        borderRadius: 6,
+        marginHorizontal: 16,
+        marginVertical: 8,
+        padding: 12,
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        backgroundColor: iOSColors.white,
+        elevation: 2,
+    }
 }
 
 const InsightComponent = ({ insightText, children }) => {
     return (
-        <View>
+        <View style={styles.view}>
             <Text style={styles.suggestionText}>
                     {
                       insightText && 
                         <Text>
                           <Text style={dashboardStyles.strong}>{`Did You Know?\n`}</Text>
-                          <Text>{`${insightText}'\n\n`}</Text>
+                          <Text>{`${insightText}`}</Text>
                         </Text>
                     }
                     {children}
