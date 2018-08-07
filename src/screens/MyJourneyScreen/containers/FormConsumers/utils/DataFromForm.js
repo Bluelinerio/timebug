b7ff9ee80257b8283758cd78f6900d803e63f34f
Graren <rennycol006@gmail.com>
2018-08-07 08:33:40 -0400

    WIP: Fixed UX issue with form2 award render
    - If user did not finish the full step, app crashed because it assumed it had all data for the render while they were actually undefined

--- src/screens/MyJourneyScreen/containers/FormConsumers/utils/DataFromForm.js
+++ src/screens/MyJourneyScreen/containers/FormConsumers/utils/DataFromForm.js
@@ -4,6 +4,8 @@ const getDataFromForm = (formData: any, wantedKeys: any) => {
   if (!formData || R.isEmpty(formData) || R.isEmpty(wantedKeys)) return {}
   return Object.keys(wantedKeys).reduce((obj, k) => {
     const { form, key } = wantedKeys[k]
+    if(!formData[form] || !formData[form][key])
+      return { ...obj }
     return {
       ...obj,
       [k]: formData[form][key]
