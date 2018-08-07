8a0fc5e975916bac8f90ff3d53c1e06cedc6d628
Graren <rennycol006@gmail.com>
2018-08-07 16:22:20 -0400

    WIP: Flow typing, formatting, and minor enhancements
    - Carousel list header building logic

--- src/screens/MyJourneyScreen/containers/FormConsumers/utils/FormModelToElement.js
+++ src/screens/MyJourneyScreen/containers/FormConsumers/utils/FormModelToElement.js
@@ -1,26 +1,38 @@
+//@flow
+import {
+  SimpleModelData,
+  Model
+}                        from '../../../../../redux/reducers/awards.reducer.js'
+import { HeaderElement } from '../../../components/GenericHeader'
 import {
   CHECKBOX,
   LABEL,
   STRUCT
-} from '../../../../../static/awards/modelTypes'
+}                        from '../../../../../static/awards/modelTypes'
 
-export const findColumnElements = ({ model }) => {
+type FindColumnElementsArgs = {
+  model: SimpleModelData
+}
+
+const isTypeRenderizable = (model: Model): boolean =>
+  model.column === true && (model.type === LABEL || model.type === CHECKBOX)
+
+const isModelStruct = (model: Model): boolean =>
+  model.type && model.type === STRUCT
+
+export const findColumnElements = ({ model }: FindColumnElementsArgs) => {
   return Object.keys(model)
     .reduce((elements, key) => {
       const mod = model[key]
-      if (
-        mod &&
-        mod.column === true &&
-        (mod.type === LABEL || mod.type === CHECKBOX)
-      )
-        return [...elements, mod]
-      else if (mod && mod.type && mod.type === STRUCT)
+      if (mod && isTypeRenderizable(mod)) return [...elements, mod]
+      else if (mod && isModelStruct(mod))
         return [...elements, ...findColumnElements({ model: mod.fields })]
+      else [...elements]
     }, [])
     .filter(model => !!model)
 }
 
-export const buildHeader = model => {
+export const buildHeader = (model: SimpleModelData): Array<HeaderElement> => {
   const columnElements = findColumnElements({ model })
   return columnElements.map(element => ({
     text: element.options.header,
