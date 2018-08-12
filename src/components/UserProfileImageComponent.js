a62bb98adb6b73623c61bb615810330740b250a7
Graren <rennycol006@gmail.com>
2018-08-12 18:51:53 -0400

    Removed MyJourney button from Banner

--- src/components/UserProfileImageComponent.js
+++ src/components/UserProfileImageComponent.js
@@ -1,17 +1,23 @@
-import React                                    from 'react'
-import { Text, View, Image, TouchableOpacity }  from 'react-native'
-import LogoutButtonContainer                    from '../containers/LogoutButtonContainer'
-import style                                    from '../styles/components/UserProfileImage'
+import React                 from 'react'
+import { View, Image }       from 'react-native'
+import LogoutButtonContainer from '../containers/LogoutButtonContainer'
 
-export default ({ onPress, styles, source }) => {
-    return (
-            <View>
-                <LogoutButtonContainer>
-                    <Image source={source} style={styles.headerAvatar} />
-                </LogoutButtonContainer>
-                <TouchableOpacity onPress={onPress} style={style.myJourneyContainer}>
-                    <Text style={style.myJourneyText}>My Journey</Text>
-                </TouchableOpacity>
-            </View>
-    )
+type UserProfileImageProps = {
+  styles: any,
+  source: string
 }
+
+const UserProfileImageComponent = ({
+  styles,
+  source
+}: UserProfileImageProps) => {
+  return (
+    <View>
+      <LogoutButtonContainer>
+        <Image source={source} style={styles.headerAvatar} />
+      </LogoutButtonContainer>
+    </View>
+  )
+}
+
+export default UserProfileImageComponent
