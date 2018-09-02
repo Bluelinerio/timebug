9ac0cf4755d4a56a5db9d163d394a5676edbddf1
OscarC <ogcol15@gmail.com>
2018-09-02 11:57:02 -0400

    Added snap animation

--- src/screens/MyJourneyScreen/components/ProgressCellComponent.js
+++ src/screens/MyJourneyScreen/components/ProgressCellComponent.js
@@ -1,13 +1,11 @@
 import React                     from 'react'
 import ProgressDashboardCell     from './ProgressCells/ProgressDashboardCell'
 import PieProgressDashboardCell  from './ProgressCells/PieProgressDashboardCell'
-import UnlockedStepsCarouselCell from './UnlockedStepsCarouselCell'
 
 const ProgressCellComponent = () => (
   <React.Fragment>
     <ProgressDashboardCell />
     <PieProgressDashboardCell />
-    <UnlockedStepsCarouselCell />
   </React.Fragment>
 )
 
--- src/screens/MyJourneyScreen/containers/MyJourneyScreenContainer.js
+++ src/screens/MyJourneyScreen/containers/MyJourneyScreenContainer.js
@@ -1,38 +1,59 @@
 // @flow
-import React                    from 'react';
-import InteractionManager       from '../../../utils/InteractionManager';
+import React                    from 'react'
+import { connect }              from 'react-redux'
+import InteractionManager       from '../../../utils/InteractionManager'
 import DefaultIndicator         from '../../../components/DefaultIndicator'
 import MyJourneyScreenComponent from '../components/MyJourneyScreenComponent'
 
 type State = {
-    didFinishInitialAnimation: boolean
-};
-
-
-class MyJourneyScreenContainer extends React.PureComponent<any, State> {
-    constructor(props) {
-        super(props);
-        this.state = {
-            didFinishInitialAnimation: false,
-        }
-    }
-
-    componentDidMount() {
-        InteractionManager.runAfterInteractions(() => {
-            this.setState({
-                didFinishInitialAnimation: true,
-            });
-        });
-    }
-
-    render() {
-        const { didFinishInitialAnimation } = this.state
-        return (
-            didFinishInitialAnimation 
-                ? <MyJourneyScreenComponent />
-                : <DefaultIndicator size="large"/>
-        )
-    }
+  didFinishInitialAnimation: boolean
 }
 
-export default MyJourneyScreenContainer;
+type Props = {
+  component: string,
+  reward: string
+}
+
+const mapStateToProps = (state: any) => {
+  const nav = state.nav
+  const params = nav.routes[0].routes[2].params
+  let component
+  let reward
+  if (params) {
+    component = params.component
+    reward = params.params.reward
+  }
+  return {
+    component,
+    reward
+  }
+}
+
+class MyJourneyScreenContainer extends React.Component<Props, State> {
+  constructor(props) {
+    super(props)
+    this.state = {
+      didFinishInitialAnimation: false
+    }
+  }
+
+  componentDidMount() {
+    InteractionManager.runAfterInteractions(() => {
+      this.setState({
+        didFinishInitialAnimation: true
+      })
+    })
+  }
+
+  render() {
+    const { didFinishInitialAnimation } = this.state
+    const { component, reward } = this.props
+    return didFinishInitialAnimation ? (
+      <MyJourneyScreenComponent component={component} reward={reward} />
+    ) : (
+      <DefaultIndicator size="large" />
+    )
+  }
+}
+
+export default connect(mapStateToProps)(MyJourneyScreenContainer)
