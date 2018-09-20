b679310e5b860cdfa1e04999cc79637bf894c491
OscarC <ogcol15@gmail.com>
2018-09-20 08:05:19 -0400

    Initial notifications set up is now a buffered action channel

--- src/redux/sagas/checkins.saga.js
+++ src/redux/sagas/checkins.saga.js
@@ -1,14 +1,19 @@
-import { takeLatest, fork, call, put, select } from 'redux-saga/effects'
-import { updateCheckin }               from '../actions/checkin.actions'
 import {
-  CHANGE_CHECKIN,
-  BUILD_NOTIFICATION_SET
-}                                      from '../actionTypes'
-import { calculateNextCheckin }        from '../../services/checkins'
-import { createNotification }          from '../actions/notifications.actions'
-import selectors                       from '../selectors'
+  takeLatest,
+  take,
+  fork,
+  actionChannel,
+  call,
+  put,
+  select
+}                                                 from 'redux-saga/effects'
+import { updateCheckin }                          from '../actions/checkin.actions'
+import { CHANGE_CHECKIN, BUILD_NOTIFICATION_SET } from '../actionTypes'
+import { calculateNextCheckin }                   from '../../services/checkins'
+import { createNotification }                     from '../actions/notifications.actions'
+import selectors                                  from '../selectors'
 
-import tron                            from 'reactotron-react-native'
+import tron from 'reactotron-react-native'
 
 function* setUpNotificationAndUpdateCheckin({ payload }) {
   const { step, frequency, message } = payload
@@ -19,18 +24,23 @@ function* setUpNotificationAndUpdateCheckin({ payload }) {
 }
 
 function* _setInitialNotifications() {
-  tron.log("Initial notifications")
+  tron.log('Initial notifications')
   const steps = yield select(selectors.steps)
   const user = yield select(selectors.user)
   tron.log(steps)
-  if(user)
-    tron.log("There is a user")
-  else
-    tron.log("No user")
+  if (user) {
+    tron.log('There is a user')
+    
+  }
+  else tron.log('No user')
 }
 
 function* watchForInitialNotifications() {
-  yield takeLatest(BUILD_NOTIFICATION_SET, _setInitialNotifications)
+  const channel = yield actionChannel(BUILD_NOTIFICATION_SET)
+  while (true) {
+    const action = yield take(channel)
+    yield call(_setInitialNotifications)
+  }
 }
 
 function* watchForCheckinsUpdate() {
