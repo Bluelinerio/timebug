2a5119186111fb5f516d5ca386c21fda88b4dc28
OscarC <ogcol15@gmail.com>
2018-09-28 11:40:30 -0400

    Added the first test to repo

---
+++ tests/components/AudioModal/AudioModal.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import AudioModal from '../../../src/components/AudioModal'
+
+describe('Testing ReassignLocationMenu component', () => {
+  it('renders as expected', () => {
+    const wrapper = shallow(
+      <AudioModal
+        isOpen={true}
+        close={() => null}
+        title={'AudioModal'}
+        audio={'https://mocked.com/some_audio'}
+      />
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+})
