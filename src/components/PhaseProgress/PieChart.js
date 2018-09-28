6a4a13983ce8583b74eddaf64670bc2c98dbe7e9
OscarC <ogcol15@gmail.com>
2018-09-28 11:56:57 -0400

    Added new linting rules + linted a few components

--- src/components/PhaseProgress/PieChart.js
+++ src/components/PhaseProgress/PieChart.js
@@ -1,63 +1,65 @@
 // @flow
-import React, { Component } from 'react'
+import React          from 'react'
 import { View, Text } from 'react-native'
-import { PieChart } from 'react-native-svg-charts'
-import styles from '../../styles/components/PieCharts'
+import { PieChart }   from 'react-native-svg-charts'
+import styles         from '../../styles/components/PieCharts'
 
 export type Slice = {
-    color: string,
-    amount: number
+  color: string,
+  amount: number
 }
-  
+
 export type Chart = {
-    label: string,
-    total: number,
-    slices: [Slice]
+  label: string,
+  total: number,
+  slices: [Slice]
 }
 
 export type ChartProps = {
-    outerRadius: string,
-    innerRadius: string,
-    spacing: number
+  outerRadius: string,
+  innerRadius: string,
+  spacing: number
 }
 
 export type PieChartProgressProps = {
-    height: number,
-    chartProps: ChartProps,
-    style?: any,
-    element: Chart
+  height: number,
+  chartProps: ChartProps,
+  style?: any,
+  element: Chart
 }
 
 type PieChartData = {
-    key: number,
-    amount: number,
-    svg?: {
-        fill?: string
-    }
+  key: number,
+  amount: number,
+  svg?: {
+    fill?: string
+  }
 }
 
-const PieChartProgress = ({ height, chartProps, style, element }: PieChartProgressProps) => {
-        const { label, total, slices } = element
+const PieChartProgress = ({
+  height,
+  chartProps,
+  element
+}: PieChartProgressProps) => {
+  const { label, slices } = element
 
-        const data: Array<PieChartData> = slices.map(({ amount, color }, index) => ({
-            key: index,
-            amount,
-            svg: { fill: color }
-        }))
+  const data: Array<PieChartData> = slices.map(({ amount, color }, index) => ({
+    key: index,
+    amount,
+    svg: { fill: color }
+  }))
 
-        return (
-            <View>
-                <PieChart
-                    style={{ height }}
-                    valueAccessor={({ item }) => item.amount}
-                    data={data}
-                    {...chartProps}
-                />
-                <Text style={styles.label}>
-                    {label}
-                </Text>
-            </View>
-        )
+  return (
+    <View>
+      <PieChart
+        style={{ height }}
+        valueAccessor={({ item }) => item.amount}
+        data={data}
+        {...chartProps}
+      />
+      <Text style={styles.label}>{label}</Text>
+    </View>
+  )
 }
 
 export default PieChartProgress
