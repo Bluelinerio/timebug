e91a06671d8e418e59d9a65eda79ddd2d9c1be37
OscarC <ogcol15@gmail.com>
2018-11-10 18:07:18 -0400

    Refactor: move backButton handling to screen component
    - Make banner also react in the same way as the back button press does

--- src/screens/PrototypeScreen/components/GoalScreenComponent.js
+++ src/screens/PrototypeScreen/components/GoalScreenComponent.js
@@ -2,11 +2,96 @@ import React                from 'react'
 import { View, ScrollView } from 'react-native'
 import styles               from '../styles'
 import { SafeAreaView }     from 'react-navigation'
+import { BackHandler }      from 'react-native'
 import Banner               from '../../../containers/NavigationAwareBanner'
 import GoalScreenContent    from '../containers/GoalScreenContentContainer'
 
-class GoalScreenComponent extends React.PureComponent<> {
+type State = {
+  selectedGoaltype: String | null,
+  selectedGoal: any | null
+}
+
+type Props = {
+  navigation: any
+}
+
+class GoalScreenComponent extends React.PureComponent<Props, State> {
+  _didFocusSubscription
+  _willBlurSubscription
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedGoaltype: null,
+      selectedGoal: null
+    }
+    this._didFocusSubscription = props.navigation.addListener('didFocus', () =>
+      BackHandler.addEventListener(
+        'hardwareBackPress',
+        this.onBackButtonPressAndroid
+      )
+    )
+  }
+
+  componentDidMount() {
+    this._willBlurSubscription = this.props.navigation.addListener(
+      'willBlur',
+      () =>
+        BackHandler.removeEventListener(
+          'hardwareBackPress',
+          this.onBackButtonPressAndroid
+        )
+    )
+  }
+
+  componentWillUnmount() {
+    this._didFocusSubscription && this._didFocusSubscription.remove()
+    this._willBlurSubscription && this._willBlurSubscription.remove()
+  }
+
+  onBackButtonPressAndroid = () => {
+    const { selectedGoaltype, selectedGoal } = this.state
+    const { navigation } = this.props
+    if (selectedGoal) {
+      this.setState({ selectedGoal: null })
+      //Animate to the left
+      return true
+    }
+    if (selectedGoaltype) {
+      this.setState({ selectedGoaltype: null })
+      // Animate to the left
+      return true
+    }
+    navigation.goBack()
+    return true
+  }
+
+  _onSoftwareBackButtonPress = () => {
+    const { selectedGoaltype, selectedGoal } = this.state
+    const { navigation } = this.props
+    if (selectedGoal) {
+      this.setState({ selectedGoal: null })
+    } else if (selectedGoaltype) {
+      this.setState({ selectedGoaltype: null })
+    } else {
+      navigation.goBack()
+    }
+  }
+
+  _onSelectGoaltype = type => {
+    this.setState({ selectedGoaltype: type })
+  }
+
+  _onSelectGoal = goal => {
+    this.setState({ selectedGoal: goal })
+  }
+
+  _unsetGoal = () => {
+    this.setState({ selectedGoal: null, selectedGoaltype: null })
+  }
+
   render() {
+    const { selectedGoaltype, selectedGoal } = this.state
     return (
       <SafeAreaView
         forceInset={{ top: 'always', bottom: 'never' }}
@@ -24,7 +109,13 @@ class GoalScreenComponent extends React.PureComponent<> {
               styles.goalScreenViewContainer
             ]}
           >
-            <GoalScreenContent />
+            <GoalScreenContent
+              onSelectGoal={this._onSelectGoal}
+              onSelectGoalType={this._onSelectGoaltype}
+              unsetGoal={this._unsetGoal}
+              selectedGoaltype={selectedGoaltype}
+              selectedGoal={selectedGoal}
+            />
           </View>
         </ScrollView>
       </SafeAreaView>
--- src/screens/PrototypeScreen/components/GoalScreenContent.js
+++ src/screens/PrototypeScreen/components/GoalScreenContent.js
@@ -1,89 +1,29 @@
 // @flow
-import React             from 'react'
-import GoalList          from './GoalList'
-import { BackHandler }   from 'react-native'
+import React from 'react'
+import GoalList from './GoalList'
 import GoalsFromTypeList from '../containers/GoalsFromTypeListContainer'
-import GoalReview        from '../containers/GoalReviewContainer'
+import GoalReview from '../containers/GoalReviewContainer'
 
-type State = {
-  selectedGoaltype: String | null,
-  selectedGoal: any | null
+type Props = {
+  onSelectGoal: () => any,
+  onSelectGoalType: () => any,
+  unsetGoal: () => any,
+  selectedGoaltype: string,
+  selectedGoal: any
 }
 
-class GoalScreenContent extends React.PureComponent<any, State> {
-  _didFocusSubscription
-  _willBlurSubscription
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      selectedGoaltype: null,
-      selectedGoal: null
-    }
-    this._didFocusSubscription = props.navigation.addListener('didFocus', () =>
-      BackHandler.addEventListener(
-        'hardwareBackPress',
-        this.onBackButtonPressAndroid
-      )
-    )
-  }
-
-  componentDidMount() {
-    this._willBlurSubscription = this.props.navigation.addListener(
-      'willBlur',
-      () =>
-        BackHandler.removeEventListener(
-          'hardwareBackPress',
-          this.onBackButtonPressAndroid
-        )
-    )
-  }
-
-  componentWillUnmount() {
-    this._didFocusSubscription && this._didFocusSubscription.remove()
-    this._willBlurSubscription && this._willBlurSubscription.remove()
-  }
-
-  onBackButtonPressAndroid = () => {
-    const { selectedGoaltype, selectedGoal } = this.state
-    const { navigation } = this.props
-    if (selectedGoal) {
-      this.setState({ selectedGoal: null })
-      //Animate to the left
-      return true
-    }
-    if (selectedGoaltype) {
-      this.setState({ selectedGoaltype: null })
-      // Animate to the left
-      return true
-    }
-    navigation.goBack()
-    return true
-  }
-
-  _onSelectGoaltype = type => {
-    this.setState({ selectedGoaltype: type })
-  }
-
-  _onSelectGoal = goal => {
-    this.setState({ selectedGoal: goal })
-  }
-
-  _unsetGoal = () => {
-    this.setState({ selectedGoal: null, selectedGoaltype: null })
-  }
-
+class GoalScreenContent extends React.PureComponent<Props> {
   render() {
-    const { selectedGoaltype, selectedGoal } = this.state
+    const { selectedGoaltype, selectedGoal } = this.props
     return (
       <React.Fragment>
         {!selectedGoaltype &&
-          !selectedGoal && <GoalList onSelect={this._onSelectGoaltype} />}
+          !selectedGoal && <GoalList onSelect={this.props.onSelectGoalType} />}
         {selectedGoaltype &&
           !selectedGoal && (
             <GoalsFromTypeList
               goal={selectedGoaltype}
-              onSelect={this._onSelectGoal}
+              onSelect={this.props.onSelectGoal}
             />
           )}
         {selectedGoaltype &&
@@ -91,7 +31,7 @@ class GoalScreenContent extends React.PureComponent<any, State> {
             <GoalReview
               goal={selectedGoal}
               type={selectedGoaltype}
-              unsetGoal={this._unsetGoal}
+              unsetGoal={this.props.unsetGoal}
             />
           )}
       </React.Fragment>
