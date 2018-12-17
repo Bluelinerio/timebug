741d999c331628cff6e1ae064db4cf7be8d49fa5
OscarC <ogcol15@gmail.com>
2018-12-17 15:23:14 -0400

    WIP: Style Guidebook

--- src/screens/v.2.0/WorkbookScreen/components/WorkbookSnippet.js
+++ src/screens/v.2.0/WorkbookScreen/components/WorkbookSnippet.js
@@ -1,27 +1,70 @@
-import React                from 'react'
-import { Text, ScrollView } from 'react-native'
-import styles               from '../styles'
-import type { Step }        from '../../../../services/cms'
+import React                              from 'react'
+import { View, Text, ScrollView }         from 'react-native'
+import styles                             from '../styles'
+import type { Step }                      from '../../../../services/cms'
+import { splitByLines }                   from '../utils/textSplit'
+import Button                             from '../../../../components/Button'
+import { connectContext }                 from 'react-connect-context'
+import { SectionConsumer, SectionValues } from '../context/SectionContext'
+import { compose, mapProps }              from 'recompose'
 
-type Props = {
+export type Props = {
   step: Step,
-  setScreenStatus: any => null,
-  model: any,
-  data: any,
+  phase: string,
+  textStyle?: any,
+  color: string,
 }
 
-class WorkbookForm extends React.PureComponent<Props> {
+type SwitchButtonProps = {
+  onPress: () => any,
+  text: string,
+  background: string,
+}
+
+const UnconnectedSwitchButton = (props: SwitchButtonProps) => {
+  const { onPress, text, background } = props
+  return (
+    <View style={styles.snippetButtonContainer}>
+      <Button backgroundColor={background} onPress={onPress} text={text} />
+    </View>
+  )
+}
+
+const merge = (props: SwitchButtonProps) => {
+  const { changeSection } = props
+  const to = SectionValues.form
+  const onPress = () => changeSection(to)
+  return {
+    ...props,
+    onPress,
+  }
+}
+
+const SwitchButton = compose(connectContext(SectionConsumer), mapProps(merge))(
+  UnconnectedSwitchButton
+)
+
+class WorkbookSnippet extends React.PureComponent<Props> {
   render() {
-    const { step } = this.props
+    const { step, color, textStyle = {} } = this.props
+    const paragraphs = splitByLines(step.description)
     return (
       <ScrollView
         style={styles.scrollView}
         contentContainerStyle={styles.scrollView}
       >
-        <Text style={styles.snippetStyle}>{step.description}</Text>
+        <View style={styles.snippetTitleContainer}>
+          <Text style={[styles.snippetTitle, textStyle]}>{step.title}</Text>
+        </View>
+        {paragraphs.map((paragraph, index) => (
+          <View key={index} style={styles.snippetParagraph}>
+            <Text style={[styles.snippetStyle, textStyle]}>{paragraph}</Text>
+          </View>
+        ))}
+        <SwitchButton text={'Next'} background={color} />
       </ScrollView>
     )
   }
 }
 
-export default WorkbookForm
+export default WorkbookSnippet
