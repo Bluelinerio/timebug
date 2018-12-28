d8a05fdd4bd75f7435a06f311de61267d0fa63fa
Eduardo <eduardoapp.97@gmail.com>
2018-12-28 01:20:05 -0400

    fix: List Component Select allows user to select emotion moroe than once

--- src/forms/custom/components/FormComponents/SelectComponent.js
+++ src/forms/custom/components/FormComponents/SelectComponent.js
@@ -2,12 +2,15 @@ import React             from 'react'
 import { View, Picker }  from 'react-native'
 import FormElementHeader from './FormElementHeader'
 import styles            from '../../styles'
+import { _stripKeys }    from './ListComponent';
+import tron from 'reactotron-react-native';
 
 const Select = ({
   value,
   onChange,
   field: { content, options },
   formStyles = {},
+  formValue,
 }: {
   value: string,
   onChange: string => any,
@@ -21,25 +24,52 @@ const Select = ({
       default: string,
     },
   },
-}) => (
-  <React.Fragment>
-    <FormElementHeader text={content.text} textStyle={formStyles.textStyle} />
-    <View style={styles.pickerContainer}>
-      <View style={[styles.pickerBackground, formStyles.elementContainerStyle]}>
-        <Picker
-          selectedValue={value ? value : options.value}
-          style={[styles.pickerStyle]}
-          onValueChange={itemValue => onChange(itemValue)}
-          itemStyle={styles.pickerItemStyle}
-        >
-          {content &&
-            content.items.map(({ value, text }) => (
-              <Picker.Item key={value} value={value} label={text} />
-            ))}
-        </Picker>
+  formValue: any,
+}) => {
+
+  const getFormItems = () => {
+    const mappedObjects = formValue && formValue.map((val) => {
+      const strippedObject = _stripKeys(val);
+      const mappedObject = Object.values(strippedObject).map(value => {
+        return value && value.value && {
+          _id: value._id,
+          value: value.value,
+        }
+      });
+
+      return mappedObject[0];
+    });
+
+    return mappedObjects;
+  };
+
+  const currentItems = getFormItems();
+
+  const filteredItems = content && content.items.filter((item) => {
+    const isSelected = currentItems && currentItems.find((ci) => ci.value === item.value);
+    return !isSelected;
+  })
+
+  return (
+    <React.Fragment>
+      <FormElementHeader text={content.text} textStyle={formStyles.textStyle} />
+      <View style={styles.pickerContainer}>
+        <View style={[styles.pickerBackground, formStyles.elementContainerStyle]}>
+          <Picker
+            selectedValue={value ? value : options.value}
+            style={[styles.pickerStyle]}
+            onValueChange={itemValue => onChange(itemValue)}
+            itemStyle={styles.pickerItemStyle}
+          >
+            {content &&
+              filteredItems.map(({ value, text }) => (
+                <Picker.Item key={value} value={value} label={text} />
+              ))}
+          </Picker>
+        </View>
       </View>
-    </View>
-  </React.Fragment>
-)
+    </React.Fragment>
+  )
+}
 
 export default Select
