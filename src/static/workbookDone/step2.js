7d9f3974e509c537646df3888326c0a6e04a60b8
Dan Kurfirst <dan.k@bluelinerny.com>
2019-01-04 17:52:21 +0000

    Revert "Merge branch 'revert-a5f2a2a3' into 'develop'"
    This reverts commit 2f5e40883b89df576d3695c0c5823e55d324519e.

---
+++ src/static/workbookDone/step2.js
@@ -0,0 +1,5 @@
+export default {
+  title: 'Congratulations!',
+  text: `You have completed Step 2: How do I want to spend my time? \n 
+      We've turned your answers into a check-in to help you stay on course. We'll remind you daily to log your time at the end of each day and then give you a report at the end of each week`,
+}
