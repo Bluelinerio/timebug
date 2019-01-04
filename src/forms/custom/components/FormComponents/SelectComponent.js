50222f78f3834ea72388d38e5a97a345f9a4eeec
OscarC <ogcol15@gmail.com>
2019-01-04 15:15:05 -0400

    Platform based selector in form components

--- src/forms/custom/components/FormComponents/SelectComponent.js
+++ src/forms/custom/components/FormComponents/SelectComponent.js
@@ -1,8 +1,51 @@
-import React             from 'react'
-import { View, Picker }  from 'react-native'
-import FormElementHeader from './FormElementHeader'
-import styles            from '../../styles'
-import { DISABLE }       from './../../forms/constants'
+import React                      from 'react'
+import { Picker, Platform, View } from 'react-native'
+import ModalSelector              from 'react-native-modal-selector'
+import FormElementHeader          from './FormElementHeader'
+import styles                     from '../../styles'
+import { DISABLE }                from './../../forms/constants'
+
+const AndroidPicker = (props: any) => {
+  const { options, items, onChange, formStyles, value } = props
+  return (
+    <View style={[styles.pickerBackground, formStyles.elementContainerStyle]}>
+      <Picker
+        selectedValue={value ? value : options.default}
+        style={[styles.pickerStyle]}
+        onValueChange={onChange}
+        itemStyle={styles.pickerItemStyle}
+      >
+        {items &&
+          items.map(({ value, text }) => (
+            <Picker.Item key={value} value={value} label={text} />
+          ))}
+      </Picker>
+    </View>
+  )
+}
+
+const IOSPicker = (props: any) => {
+  const { items, onChange, formStyles, value } = props
+
+  const data =
+    items &&
+    items.map(({ value, text }) => ({
+      key: value,
+      label: text,
+    }))
+
+  return (
+    data && (
+      <View style={[styles.pickerStyle, formStyles.elementContainerStyle]}>
+        <ModalSelector
+          initValue={value ? value : data[0].key}
+          data={data}
+          onChange={onChange}
+        />
+      </View>
+    )
+  )
+}
 
 const Select = ({
   value,
@@ -30,6 +73,14 @@ const Select = ({
     filterFunction?: (Array<any>) => Array<any>,
   },
 }) => {
+  const onValueChange = itemValue => {
+    onChange(itemValue)
+  }
+
+  const onIosChange = item => {
+    onChange(item.key)
+  }
+
   const { filterFunction = null } = __extraProps
   const filteredItems =
     content && content.items
@@ -45,21 +96,23 @@ const Select = ({
     <React.Fragment>
       <FormElementHeader text={content.text} textStyle={formStyles.textStyle} />
       <View style={styles.pickerContainer}>
-        <View
-          style={[styles.pickerBackground, formStyles.elementContainerStyle]}
-        >
-          <Picker
-            selectedValue={value ? value : options.default}
-            style={[styles.pickerStyle]}
-            onValueChange={itemValue => onChange(itemValue)}
-            itemStyle={styles.pickerItemStyle}
-          >
-            {filteredItems &&
-              filteredItems.map(({ value, text }) => (
-                <Picker.Item key={value} value={value} label={text} />
-              ))}
-          </Picker>
-        </View>
+        {Platform.OS === 'ios' ? (
+          <IOSPicker
+            value={value}
+            formStyles={formStyles}
+            items={filteredItems}
+            options={options}
+            onChange={onIosChange}
+          />
+        ) : (
+          <AndroidPicker
+            value={value}
+            formStyles={formStyles}
+            items={filteredItems}
+            options={options}
+            onChange={onValueChange}
+          />
+        )}
       </View>
     </React.Fragment>
   )
