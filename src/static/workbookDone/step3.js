dc60c27e2a4f2397738daea2ffd3e3b3be5dc64b
Eduardo <eduardoapp.97@gmail.com>
2019-01-05 18:31:33 -0400

    fix: deleting <b> tag on step3 end screen

--- src/static/workbookDone/step3.js
+++ src/static/workbookDone/step3.js
@@ -1,6 +1,6 @@
 export default {
   title: 'Congratulations!',
-  text: `You have completed <b>Step 3: What are my
-    Strengths and Weaknesses?</b> \n
+  text: `You have completed Step 3: What are my
+    Strengths and Weaknesses? \n
     This is a great excercise in self honesty and transparency. Keep it going!`,
 }
