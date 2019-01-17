92ac0e7618959344873ceddf84e80307477e1511
OscarC <ogcol15@gmail.com>
2019-01-16 23:13:08 -0400

    Feat: changed colors for phase 2 incomplete elements
    - Added background color to statusbar as well
    - Fixed weird color behavior between complete and incomplete steps

--- src/containers/PhaseHeaderContainer.js
+++ src/containers/PhaseHeaderContainer.js
@@ -1,89 +1,87 @@
 // @flow
-import { NavigationActions } from 'react-navigation';
-import { connect } from 'react-redux';
-
-import PhaseHeader from '../components/PhaseHeader';
-import type { Props } from '../components/PhaseHeader';
-import selectors from '../redux/selectors';
-import { formatPhaseTitle, translateCMSPhaseToStandard } from '../services/cms';
-import type { Step } from '../services/cms';
+import { NavigationActions }                             from 'react-navigation'
+import { connect }                                       from 'react-redux'
+import PhaseHeader                                       from '../components/PhaseHeader'
+import type { Props }                                    from '../components/PhaseHeader'
+import selectors                                         from '../redux/selectors'
+import { formatPhaseTitle, translateCMSPhaseToStandard } from '../services/cms'
+import type { Step }                                     from '../services/cms'
 import {
   getTextColorFromStep,
   backgroundColorFromStep,
   getColorFromStep,
   phaseHeaderBackgroundColorForPhase,
-} from '../styles/components/global';
+}                                                        from '../styles/components/global'
 
 type DispatchProps = {
   goBack: () => any,
-};
+}
 
 type OwnProps = {
   override: () => any,
   step: Step,
   phase: string,
   onSelectStep: Step => any,
-};
+}
 
 type StateProps = {
   steps: Array<Step>,
   user: any,
   isStepCompleted: number => boolean,
-};
+}
 
 const mapStateToProps = (state: any) => {
-  const steps = Object.values(selectors.steps(state));
-  const user = selectors.getUser(state);
-  const isStepCompleted = selectors.isStepCompleted(state);
+  const steps = Object.values(selectors.steps(state))
+  const user = selectors.getUser(state)
+  const isStepCompleted = selectors.isStepCompleted(state)
   return {
     steps,
     user,
     isStepCompleted,
-  };
-};
+  }
+}
 
 const mapDispatchToProps = (dispatch: any): DispatchProps => ({
   goBack: () => dispatch(NavigationActions.back()),
-});
+})
 
 const mergeProps = (
   stateProps: StateProps,
   dispatchProps: DispatchProps,
   ownProps: OwnProps
 ): Props => {
-  const { isStepCompleted, steps } = stateProps;
-  const { override, step, phase, ...rest } = ownProps;
-  const { type, stepId } = step;
-  const { goBack } = dispatchProps;
+  const { isStepCompleted, steps } = stateProps
+  const { override, step, phase, ...rest } = ownProps
+  const { type, stepId } = step
+  const { goBack } = dispatchProps
 
-  const title = formatPhaseTitle(type);
-  const onBackPress = override ? override : goBack;
+  const title = formatPhaseTitle(type)
+  const onBackPress = override ? override : goBack
+  const headerBackgroundColor = phaseHeaderBackgroundColorForPhase(phase)
 
-  const textColor = getTextColorFromStep(step.type, isStepCompleted(stepId));
-  const backgroundColor = backgroundColorFromStep(
-    step.type,
-    isStepCompleted(stepId)
-  );
-
-  const headerBackgroundColor = phaseHeaderBackgroundColorForPhase(phase);
-
-  const titleColor = getColorFromStep(step.type);
+  const titleColor = getColorFromStep(step.type)
 
   return {
     ...rest,
     onBackPress,
     backButton: true,
     title,
-    textColor,
-    backgroundColor,
     titleColor,
     headerBackgroundColor,
-    steps: steps.filter(
-      step => translateCMSPhaseToStandard(step.type) === phase
-    ),
-  };
-};
+    steps: steps
+      .filter(step => translateCMSPhaseToStandard(step.type) === phase)
+      .map(step => {
+        const isCompleted = isStepCompleted(step.number)
+        step._buttonBackgroundColor = backgroundColorFromStep(
+          step.type,
+          isCompleted
+        )
+        step._textColor = getTextColorFromStep(step.type, isCompleted)
+        return step
+      }),
+  }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(
   PhaseHeader
-);
+)
