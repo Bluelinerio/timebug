db2e9862a304e7ee6c4ca2b1d77d1f904349cfae
OscarC <ogcol15@gmail.com>
2019-03-05 22:29:04 -0400

    WIP
    WIP: Remove strip keys on slider set
    
    
    Fix: Add local childType options to options
    
    
    Feat: List component refactored. Extracted common APIS
    
    
    Feat: Extract text component from List component file

--- src/forms/custom/components/FormComponents/FormElementsComponent.js
+++ src/forms/custom/components/FormComponents/FormElementsComponent.js
@@ -1,12 +1,10 @@
-import React             from 'react'
-import { View }          from 'react-native'
-import uuid              from 'uuid/v4'
-import styles            from '../../styles'
-import FormElementHeader from './FormElementHeader'
-import FormPicker        from './FormPicker'
-import { DISABLE }       from './../../forms/constants'
-import types             from '../../forms/types'
-import { stripKeys }     from '../../utils/stripKeys'
+import React                 from 'react'
+import { View }              from 'react-native'
+import uuid                  from 'uuid/v4'
+import styles                from '../../styles'
+import FormElementHeader     from './FormElementHeader'
+import FormPicker            from './FormPicker'
+import { buildInitialValue } from '../../utils/formHelpers'
 
 type Props = {
   value: Array<any>,
@@ -24,7 +22,7 @@ class FormElementsComponent extends React.PureComponent<Props, State> {
   componentDidUpdate = prevProps => {
     const { field, value, onChange } = this.props
     if (!value && field.key !== prevProps.key) {
-      const initialValue = this._buildInitialValue(this.props)
+      const initialValue = buildInitialValue(this.props)
       onChange({ ...initialValue, _id: uuid() })
     }
   }
@@ -32,86 +30,9 @@ class FormElementsComponent extends React.PureComponent<Props, State> {
   componentDidMount = () => {
     const { value, onChange } = this.props
     if (!value) {
-      const initialValue = this._buildInitialValue(this.props)
+      const initialValue = buildInitialValue(this.props)
       onChange({ ...initialValue, _id: uuid() })
     }
-  }
-
-  _buildInitialValue = (props: Props) => {
-    const { field: { options } } = props
-    const { childTypes } = options
-    const initialValue = Object.keys(childTypes).reduce((value, index) => {
-      const child = childTypes[index]
-      const { key, type } = child
-      return type === types.select &&
-        options &&
-        options.repeats &&
-        options.repeats === DISABLE
-        ? {
-          ...value,
-          [key]: this._handleSelectTypeValue(child, index, props),
-        }
-        : {
-          ...value,
-          [key]: {
-            key,
-            index,
-            _model: child,
-            _id: uuid(),
-            value: child.options ? child.options.default : undefined,
-          },
-        }
-    }, {})
-    return initialValue
-  }
-
-  _handleSelectTypeValue = (child, indexKey, props) => {
-    const { key, content: { items = [] } } = child
-    const filteredValues = this._selectFilterRoot(props, items)
-    return {
-      key,
-      index: indexKey,
-      _model: child,
-      _id: uuid(),
-      value:
-        filteredValues && filteredValues.length > 0
-          ? filteredValues[0].value
-          : undefined,
-    }
-  }
-
-  _selectFilterRoot = (props, items) => {
-    const { value } = props
-    const currentItems = this._getFormItems(value)
-    const filtered = items
-      ? items.filter(item => {
-        const filter = ci => ci.value === item.value
-        const isSelected = currentItems && currentItems.find(filter)
-        return !isSelected
-      })
-      : items
-    return filtered
-  }
-
-  _getFormItems = formValue => {
-    const mappedObjects =
-      formValue &&
-      formValue.map(val => {
-        const strippedObject = stripKeys(val)
-        const mappedObject = Object.values(strippedObject).map(value => {
-          return (
-            value &&
-            value.value && {
-              _id: value._id,
-              value: value.value,
-            }
-          )
-        })
-
-        return mappedObject[0]
-      })
-
-    return mappedObjects
   }
 
   _onChange = key => changedValue => {
