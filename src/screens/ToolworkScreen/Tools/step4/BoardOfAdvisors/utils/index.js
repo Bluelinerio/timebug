a1172c4551826f192fd4e4b45ba06b67a915f4a4
OscarC <ogcol15@gmail.com>
2019-03-12 12:18:24 -0400

    Fix: Remove coercion of undefined to string in tool 4

--- src/screens/ToolworkScreen/Tools/step4/BoardOfAdvisors/utils/index.js
+++ src/screens/ToolworkScreen/Tools/step4/BoardOfAdvisors/utils/index.js
@@ -2,6 +2,6 @@ import { Platform } from 'react-native'
 
 export const getContactName = (contact: any) => {
   return Platform.OS === 'ios'
-    ? `${contact.fullName}`
-    : `${contact.displayName}`
+    ? contact.fullName ? `${contact.fullName}` : undefined
+    : contact.displayName ? `${contact.displayName}` : undefined
 }
