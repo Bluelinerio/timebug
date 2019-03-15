db611bcfb59f491c3b176da4960acc63ea94708b
Daniel Kurfirst <danielkurfirst@Daniels-MacBook-Pro.local>
2019-03-15 14:33:53 -0400

    Done text for step 6 and step 7

---
+++ src/static/workbookDone/step6.js
@@ -0,0 +1,5 @@
+export default {
+    title: 'Congratulations!',
+    text: 'Nice job, you have finished step 6. You now have access to the "Goals of Others" tool that will allow you to track what you can do to help those close to you. By helping others, we are reminded of the interconnectedness of our families, communities, and the entire world. Not to mention, empowering those around us strengthens our own support network. Giving and reciprocity are essential to the 20/20 Life Vision path.' ,
+  }
+  
