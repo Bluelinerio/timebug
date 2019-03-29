ddc063a174c14ec83843dde12060b40d1b565083
OscarC <ogcol15@gmail.com>
2019-03-29 17:44:14 -0400

    WIP: Goals backlog
    TODO: everything, it's a copy paste of the old goals completed screen, skipping to another task before this one

---
+++ src/screens/ToolworkScreen/Tools/step5/GoalBacklog/components/BackloggedGoalDetails.js
@@ -0,0 +1,95 @@
+// @flow
+import React                            from 'react'
+import moment                           from 'moment'
+import { View, Text, TouchableOpacity } from 'react-native'
+import styles                           from '../../common/styles'
+import { DATE_FORMAT }                  from '2020_constants/constants'
+import type { GoalWithToolData }        from '../../common/types'
+
+type Props = {
+  goal: GoalWithToolData,
+  toggleGoal: () => any,
+  deleteGoal: () => any,
+  unsetGoal: () => any,
+  title: string,
+  dialogElements: Array<any>,
+  completionDate: string,
+  goalOutcome: string,
+}
+
+class GoalReview extends React.PureComponent<Props> {
+  state = {
+    openDialog: false,
+  }
+
+  _deleteGoal = () => {
+    const { deleteGoal } = this.props
+    deleteGoal()
+  }
+
+  _onClose = () => {
+    this.setState({ openDialog: false })
+  }
+
+  _openDialog = () => {
+    this.setState({ openDialog: true })
+  }
+
+  render() {
+    const {
+      toggleGoal,
+      title,
+      dialogElements,
+      completionDate,
+      goal,
+      goalOutcome,
+    } = this.props
+    return (
+      <React.Fragment>
+        <View style={styles.titleContainer}>
+          <Text style={styles.goalScreenSubtitle}>{title}</Text>
+        </View>
+        <View style={styles.container}>
+          <View style={[styles.goalReviewTextBlock, styles.goalReviewIndent]}>
+            <Text style={styles.goalScreenContent}>
+              Completed at: {completionDate}
+            </Text>
+          </View>
+          <View style={[styles.goalReviewTextBlock, styles.goalReviewIndent]}>
+            <Text style={styles.goalScreenContent}>
+              Created at: {moment(goal.created_at).format(DATE_FORMAT)}
+            </Text>
+          </View>
+          <TouchableOpacity onPress={this._openDialog}>
+            <Text>
+              Goal Outcome:{' '}
+              {goalOutcome
+                ? dialogElements.find(e => e.key === goalOutcome).text
+                : 'Not selected'}
+            </Text>
+          </TouchableOpacity>
+          <View style={styles.goalReviewTextWithMargin}>
+            <Text style={styles.goalTimeLeft}>
+              Congratulations completing this goal!
+            </Text>
+          </View>
+          <View style={styles.optionsContainer}>
+            <TouchableOpacity style={styles.optionButton} onPress={toggleGoal}>
+              <Text style={styles.optionButtonText}>Re open</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.optionsContainer}>
+            <TouchableOpacity
+              style={styles.optionButton}
+              onPress={this._deleteGoal}
+            >
+              <Text style={styles.optionButtonText}>Delete</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </React.Fragment>
+    )
+  }
+}
+
+export default GoalReview
---
+++ src/screens/ToolworkScreen/Tools/step5/GoalBacklog/containers/BackloggedGoalDetailsContainer.js
@@ -0,0 +1,147 @@
+// @flow
+import moment                            from 'moment'
+import { connect }                       from 'react-redux'
+import { withNavigation }                from 'react-navigation'
+import { compose }                       from 'recompose'
+import selectors                         from '2020_redux/selectors'
+import { FORM_KEYS }                     from '2020_forms/forms/goals'
+import { DATE_FORMAT, TEXT_DATE_FORMAT } from '2020_constants/constants'
+import { CommonGoalOutcomesArray }       from '2020_forms/forms/content'
+import { deleteSingleFormElement }       from '2020_redux/actions/formData.actions'
+import type { DeleteFormValuePayload }   from '2020_redux/actions/formData.actions'
+import BackloggedGoalDetails             from '../components/BackloggedGoalDetails'
+import { stepEnum }                      from '2020_services/cms'
+
+type StateProps = {
+  formData: {
+    [x: string]: {
+      _id: string,
+      value: Array<any>,
+    },
+  },
+}
+
+type OwnProps = {
+  tool: { subtitle: string },
+  storeAwardData: (any, any) => any,
+  data: { value: Array<any>, _id?: string, date?: string },
+  goal: any,
+  type: string,
+  unsetGoal: () => any,
+}
+
+const mapStateToProps = (state: any): StateProps => {
+  const formData = selectors.formData(state)
+  return {
+    formData,
+  }
+}
+
+const mapDispatchToProps = (dispatch: any) => ({
+  deleteGoal: (payload: DeleteFormValuePayload) =>
+    dispatch(deleteSingleFormElement(payload)),
+})
+
+const toggleSoftDelete = (
+  goal,
+  currentAwardData,
+  tool,
+  storeAwardData,
+  unsetGoal
+) => {
+  return () => {
+    const { _id } = goal
+    const value = currentAwardData ? currentAwardData.value || [] : []
+
+    const oldData = value.find(
+      goalAwardData => goalAwardData.goalId === _id
+    ) || {
+        createdAt: moment().format(DATE_FORMAT),
+        goalId: _id,
+      }
+
+    const newGoalAwardValue = {
+      ...oldData,
+      updatedAt: moment().format(DATE_FORMAT),
+      deleted: !oldData.completed,
+      deletionDate:
+        !oldData.deleted === true ? moment().format(TEXT_DATE_FORMAT) : null,
+    }
+
+    const newData = [
+      ...value.filter(val => val.goalId !== _id),
+      newGoalAwardValue,
+    ]
+
+    storeAwardData(newData, tool)
+    unsetGoal()
+  }
+}
+
+const fullyDeleteGoal = (
+  goal,
+  currentAwardData,
+  tool,
+  storeAwardData,
+  callDeleteGoal,
+  unsetGoal
+) => {
+  return () => {
+    const { _id } = goal
+    const value = currentAwardData ? currentAwardData.value || [] : []
+    const step5 = stepEnum.STEP_5
+
+    const newData = [...value.filter(val => val.goalId !== _id)]
+
+    storeAwardData(newData, tool)
+    unsetGoal()
+
+    callDeleteGoal({
+      stepId: step5,
+      id: _id,
+    })
+  }
+}
+
+const merge = (
+  stateProps: StateProps,
+  dispatchProps: any,
+  ownProps: OwnProps
+) => {
+  const { goal, data, storeAwardData, tool, unsetGoal } = ownProps
+  const { deleteGoal: callDeleteGoal } = dispatchProps
+  const completionDate = goal.toolData.completionDate
+  const title = goal[FORM_KEYS.form_5_recent_life_goals].value || ''
+  const toggleGoal = toggleSoftDelete(
+    goal,
+    data,
+    tool,
+    storeAwardData,
+    unsetGoal
+  )
+  const deleteGoal = fullyDeleteGoal(
+    goal,
+    data,
+    tool,
+    storeAwardData,
+    callDeleteGoal,
+    unsetGoal
+  )
+  const goalOutcome = goal.toolData.goalOutcome
+  const dialogElements = CommonGoalOutcomesArray
+  return {
+    goal,
+    toggleGoal,
+    unsetGoal,
+    deleteGoal,
+    title,
+    dialogElements,
+    completionDate,
+    goalOutcome,
+  }
+}
+
+export default compose(
+  withNavigation,
+  connect(mapStateToProps, mapDispatchToProps, merge)
+)(BackloggedGoalDetails)
