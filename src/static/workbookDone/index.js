5c7b229477cbbb379119bd326130ad5952f56604
Daniel Kurfirst <danielkurfirst@Daniels-MacBook-Pro.local>
2019-05-09 11:53:09 -0400

    fix info.plist, add step8 done text

--- src/static/workbookDone/index.js
+++ src/static/workbookDone/index.js
@@ -5,6 +5,7 @@ import STEP_4 from './step4'
 import STEP_5 from './step5'
 import STEP_6 from './step6'
 import STEP_7 from './step7'
+import STEP_8 from './step8'
 
 export default {
   '1': STEP_1,
@@ -13,5 +14,6 @@ export default {
   '4': STEP_4,
   '5': STEP_5,
   '6': STEP_6,
-  '7': STEP_7
+  '7': STEP_7,
+  '8': STEP_8
 }
---
+++ src/static/workbookDone/step8.js
@@ -0,0 +1,5 @@
+export default {
+    title: 'Congratulations!',
+    text: 'Way to go! You have completed Step 8! You have unlocked the "My Energy Levels" tool, which will allow you to check in up to 6 times per day and let us know how you are feeling. In return, you will get back daily, weekly and monthly distribution charts that will show you how your energy flows. This can help show you where you are doing well and where there is room for improvement.',
+  }
+  
