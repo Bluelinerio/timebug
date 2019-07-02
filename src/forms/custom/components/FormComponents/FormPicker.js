13d783d369f0158dcb7f64a5d5cd004b2d4ae36e
Dan Kurfirst <dan.k@bluelinerny.com>
2019-07-02 19:27:05 +0000

    Merge branch '410-create-forms-for-step-9-and-10' into 'develop'
    Resolve "Create Forms for step 9 and 10"
    
    Closes #410
    
    See merge request timebug/2020-react-native!277

--- src/forms/custom/components/FormComponents/FormPicker.js
+++ src/forms/custom/components/FormComponents/FormPicker.js
@@ -1,14 +1,16 @@
+// @flow
 import React          from 'react'
+import types          from '../../forms/types'
 import Label          from './Label'
 import TextInput      from './TextInput'
 import Button         from './ButtonComponent'
-import types          from '../../forms/types'
 import Select         from './SelectComponent'
 import List           from './ListComponent'
 import MultipleSelect from './MultipleSelectComponent'
 import Set            from './SetComponent'
 import Slider         from './SingleSlider'
 import FormElements   from './FormElementsComponent'
+import BooleanSwitch  from './SwitchComponent'
 import { connect }    from './ConnectedComponentContainer'
 
 type Props = {
@@ -22,26 +24,28 @@ type Props = {
 const SwitchComponent = ({ field, props }: { type: string, props: any }) => {
   const { type } = field
   switch (type) {
-  case types.label:
-    return <Label {...props} />
-  case types.string:
-    return <TextInput {...props} />
-  case types.button:
-    return <Button {...props} />
-  case types.select:
-    return <Select {...props} />
-  case types.list:
-    return <List {...props} />
-  case types.multipleSelect:
-    return <MultipleSelect {...props} />
-  case types.set:
-    return <Set {...props} />
-  case types.slider:
-    return <Slider {...props} />
-  case types.formElements:
-    return <FormElements {...props} />
-  default:
-    return null
+    case types.label:
+      return <Label {...props} />
+    case types.string:
+      return <TextInput {...props} />
+    case types.button:
+      return <Button {...props} />
+    case types.select:
+      return <Select {...props} />
+    case types.list:
+      return <List {...props} />
+    case types.multipleSelect:
+      return <MultipleSelect {...props} />
+    case types.set:
+      return <Set {...props} />
+    case types.slider:
+      return <Slider {...props} />
+    case types.formElements:
+      return <FormElements {...props} />
+    case types.switch:
+      return <BooleanSwitch {...props} />
+    default:
+      return null
   }
 }
 
