6d008390ccdf440bd84af883b153d4ba9690eefa
Osc <rennycol006Q>
2019-07-25 00:56:52 +0200

    Restore Firebase JS side

--- src/containers/AppNavigationWithModal.js
+++ src/containers/AppNavigationWithModal.js
@@ -1,7 +1,8 @@
 import AppNavigation         from '../navigation/app'
 import { compose }           from 'recompose'
 import { withNotifications } from './NotificationsContainer'
+import { withFirebase } from './FirebaseContainer'
 
-export default compose(withNotifications)(
+export default compose(withNotifications, withFirebase)(
   AppNavigation
 )
