2a6f7e83e994d7f39de2124352a337a42992b953
Graren <ogcol15@gmail.com>
2019-08-28 00:43:35 +0200

    GOals screen tool 23 view

--- src/screens/GoalScreen/v2/components/VisionCreationComponent.js
+++ src/screens/GoalScreen/v2/components/VisionCreationComponent.js
@@ -1,15 +1,3 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import styles from '../styles'
-
-class VisionCreationComponent extends React.PureComponent {
-  render() {
-    return (
-      <View style={styles.lockedContainer}>
-        <Text style={styles.lockedText}>Check in back later to use this tool!</Text>
-      </View>
-    )
-  }
-}
+import VisionCreationComponent from './VisionCreation'
 
 export default VisionCreationComponent
