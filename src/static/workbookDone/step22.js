7ab025a66b6faebd95a26b8ae3dc0ff369cad8b0
Dan Kurfirst <dan@blueliner.io>
2019-09-04 13:05:59 -0400

    rest of done screens

--- src/static/workbookDone/step22.js
+++ src/static/workbookDone/step22.js
@@ -3,7 +3,7 @@ import { WorkbookDoneConfig } from '2020_types/types'
 
 const workBookDoneConfig: WorkbookDoneConfig = {
   title: 'Congratulations!',
-  text: 'You have completed Step 22! Follow up with step 23!',
+  text: 'What we dream about during sleep often reveals the inner workings of our subconcious. Some dreams are even specific enough to point us towards real-life, tangible goals. If you have not already, you may now begin your Vision Creation assesment with step 23.',
 }
 
 export default workBookDoneConfig
