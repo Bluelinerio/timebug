03210505cf799c2a11602a0485f209eadb973962
OscarC <ogcol15@gmail.com>
2019-09-12 16:33:56 -0300

    Display goal count on goals screen

--- src/screens/GoalScreen/v2/containers/PhaseListContainer.js
+++ src/screens/GoalScreen/v2/containers/PhaseListContainer.js
@@ -1,7 +1,10 @@
 import React, { useContext, memo, useMemo } from 'react'
+import { useSelector, shallowEqual } from 'react-redux'
+import selectors from '2020_redux/selectors'
 import PhaseList from '../components/PhaseList'
 import { PhaseContext } from '../context/PhaseContext'
 import { getStyleForPhase } from '../utils/styleForPhase'
+import { getUnlockedTools } from '2020_services/tools'
 
 const PhaseListContainer = () => {
   const { phase } = useContext(PhaseContext)
@@ -15,7 +18,17 @@ const PhaseListContainer = () => {
     },
     [phase]
   )
-  return <PhaseList style={style} />
+
+  const completedSteps = useSelector(selectors.getCompletedSteps, shallowEqual)
+
+  const unlockedTools = useMemo(
+    () => {
+      return getUnlockedTools(completedSteps)
+    },
+    [completedSteps]
+  )
+
+  return <PhaseList style={style} unlockedTools={unlockedTools} />
 }
 
 export default memo(PhaseListContainer)
