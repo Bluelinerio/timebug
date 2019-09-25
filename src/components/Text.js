daaf1fca58bc574db6648bc0e06dcaf58b47c03d
Graren <ogcol15@gmail.com>
2019-09-25 03:35:39 +0200

    FIx?

--- src/components/Text.js
+++ src/components/Text.js
@@ -3,10 +3,13 @@ import React from 'react'
 import { Text } from 'react-native'
 import styles from '../styles/components/Text'
 
-const WrappedText = (props: any) => {
-  const { style, ...rest } = props
+const WrappedText = (props: any = {}) => {
+  const { style } = props
+  const st = Array.isArray(style)
+    ? [styles.text, ...style]
+    : typeof style === 'object' ? [styles.text, style] : styles.text
   return (
-    <Text {...rest} style={[styles.text, ...style]}>
+    <Text {...props} style={st}>
       {props.children}
     </Text>
   )
