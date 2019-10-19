a8448dbab3d07535ec00383fd4a9dc97968c1e63
Graren <ogcol15@gmail.com>
2019-10-19 05:07:37 +0200

    Add new markdown package

--- src/screens/v.2.0/WorkbookScreen/components/WorkbookSnippet.js
+++ src/screens/v.2.0/WorkbookScreen/components/WorkbookSnippet.js
@@ -1,11 +1,13 @@
-import React                              from 'react'
-import { View, Text, ScrollView, Image }  from 'react-native'
-import { compose, mapProps }              from 'recompose'
-import Button                             from '../../../../components/Button'
-import type { Step }                      from '../../../../services/cms'
+// @flow
+import React, { useMemo } from 'react'
+import { View, Text, ScrollView, Image } from 'react-native'
+import { compose, mapProps } from 'recompose'
+import Markdown from 'react-native-markdown-renderer'
+import Button from '../../../../components/Button'
+import type { Step } from '../../../../services/cms'
 import { SectionValues } from '../context/SectionContext'
-import styles                             from '../styles'
-import { headerBackgrounds }              from '../../../../resources/images'
+import styles from '../styles'
+import { headerBackgrounds } from '../../../../resources/images'
 
 export type Props = {
   step: Step,
@@ -41,37 +43,65 @@ const merge = (props: SwitchButtonProps) => {
   }
 }
 
-const SwitchButton = compose(mapProps(merge))(
-  UnconnectedSwitchButton
-)
+const SwitchButton = compose(mapProps(merge))(UnconnectedSwitchButton)
 
-class WorkbookSnippet extends React.PureComponent<Props> {
-  render() {
-    const { step, color, textStyle = {}, changeSection, backgroundColor } = this.props
-    return (
-      <ScrollView
-        style={styles.scrollView}
-        contentContainerStyle={styles.scrollView}
-      >
-        <View style={styles.marginScrollViewElement}>
-          <View style={styles.actualSnippetContainer}>
-            <Text style={[styles.actualSnippetText, textStyle]}>
-              {step.snippet}
-            </Text>
-          </View>
-          <View style={styles.snippetParagraph}>
-            <Text style={[styles.snippetStyle, textStyle]}>
-              {step.description}
-            </Text>
-          </View>
-          <SwitchButton text={'Next'} background={color} changeSection={changeSection} />
-          <View style={[styles.backgroundImageContainer]}>
-            <Image source={headerBackgrounds[step.number]} style={[styles.backgroundImage, { tintColor: backgroundColor }]}/>
-          </View>
+const WorkbookSnippet = (props: Props) => {
+  const { step, color, textStyle = {}, changeSection, backgroundColor } = props
+
+  const changedMarkdownStyles = useMemo(
+    () => {
+      return {
+        text: {
+          ...textStyle,
+          fontFamily: 'Metropolis',
+          fontSize: 15,
+        },
+        paragraph: {
+          ...textStyle,
+          fontFamily: 'Metropolis',
+          marginVertical: 6,
+        },
+        u: {
+          ...textStyle,
+          fontFamily: 'Metropolis',
+          fontWeight: '700',
+        },
+        strong: {
+          fontFamily: 'Metropolis',
+          fontWeight: '700',
+        },
+      }
+    },
+    [textStyle]
+  )
+  return (
+    <ScrollView
+      style={styles.scrollView}
+      contentContainerStyle={styles.scrollView}
+    >
+      <View style={styles.marginScrollViewElement}>
+        <View style={styles.actualSnippetContainer}>
+          <Text style={[styles.actualSnippetText, textStyle]}>
+            {step.snippet}
+          </Text>
         </View>
-      </ScrollView>
-    )
-  }
+        <View style={styles.snippetParagraph}>
+          <Markdown style={changedMarkdownStyles}>{step.description}</Markdown>
+        </View>
+        <SwitchButton
+          text={'Next'}
+          background={color}
+          changeSection={changeSection}
+        />
+        <View style={[styles.backgroundImageContainer]}>
+          <Image
+            source={headerBackgrounds[step.number]}
+            style={[styles.backgroundImage, { tintColor: backgroundColor }]}
+          />
+        </View>
+      </View>
+    </ScrollView>
+  )
 }
 
-export default WorkbookSnippet
+export default React.memo(WorkbookSnippet)
--- src/styles/Markdown/assignment.js
+++ src/styles/Markdown/assignment.js
@@ -1,22 +1,11 @@
-//import { hotPink } from '../../constants/colors';
-const hotPink = 'rgba(236, 0, 140, 0.72)';
-
 export default {
   u: {
-    fontWeight: 'bold',
-    fontFamily: 'Helvetica',
-    color: hotPink,
+    fontWeight: '700',
+    fontFamily: 'Metropolis',
   },
   block: {
     textAlign: 'left',
-    fontWeight: '300',
-    fontFamily: 'Helvetica',
-    fontSize: 25,
-    marginBottom: 15,
-  },
-  listItemContent: {
-    textAlign: 'left',
-    fontFamily: 'Helvetica',
-    fontSize: 18,
+    fontWeight: '500',
+    fontFamily: 'Metropolis',
   },
 };
