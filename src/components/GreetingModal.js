3c65f55645391806afcf164f2dc6d8008da22243
Daniel Kurfirst <danielkurfirst@Daniels-MBP.home>
2020-01-03 12:39:32 -0500

    reformat modal, remove user_friends from facebook service and reinstall pods

--- src/components/GreetingModal.js
+++ src/components/GreetingModal.js
@@ -1,5 +1,5 @@
 import React, { useState, useCallback } from 'react'
-import { View, Text, Modal, Image, TouchableOpacity } from 'react-native'
+import { View, Text, Modal, Image, TouchableOpacity, ImageBackground } from 'react-native'
 import styles, { gradientColors } from '../styles/components/GreetingModal'
 import src from '../resources/images/Timebug-hourglass-1.png'
 import Icon from 'react-native-vector-icons/Ionicons'
@@ -26,6 +26,11 @@ const GreetingModal = () => {
         <View style={styles.titleContainer}>
           <Text style={styles.title}>
             YOUR TIME IS YOUR ENERGY IS YOUR LIFE. USE IT WISELY
+          </Text>
+          <Text style={styles.body}>
+            You are what you spend your time on. Optimize your 'Energetic Investments' in a way that is in sync with your vision. One of the keys here 
+            is minimizing distractions which eat up chunks of time that could have
+            been highly productive, but otherwise get lost in the hourglass of time which you can never get back.
           </Text>
         </View>
         <View style={styles.imageContainer}>
